c6a54c3d975ad1453dd04082eab1db762ead5182
MellyGray <melinaabril@gmail.com>
2023-02-15 10:47:46 +0100

    feat(tests): add Router to unit tests

--- tests/sections/hello-dataverse/HelloDataverse.test.tsx
+++ tests/sections/hello-dataverse/HelloDataverse.test.tsx
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { HelloDataverse } from '../../../src/sections/hello-dataverse/HelloDataverse'
+import { renderWithRouter } from '../../renderWithRouter'
 
 test('renders hello dataverse title', () => {
-  render(<HelloDataverse />)
+  renderWithRouter(<HelloDataverse />)
+
   const titleElement = screen.getByText(/hello dataverse/i)
   expect(titleElement).toBeInTheDocument()
 })
