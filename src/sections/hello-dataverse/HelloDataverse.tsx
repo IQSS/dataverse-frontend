401ad7af94cb43c718364a33ff10944baaff6243
MellyGray <melinaabril@gmail.com>
2023-02-15 14:21:31 +0100

    feat(i18n): add more complex translations with Trans

--- src/sections/hello-dataverse/HelloDataverse.tsx
+++ src/sections/hello-dataverse/HelloDataverse.tsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg'
 import styles from './HelloDataverse.module.scss'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 export function HelloDataverse() {
   const { t } = useTranslation('helloDataverse')
@@ -9,9 +9,9 @@ export function HelloDataverse() {
     <div className={styles.container}>
       <header>
         <h1 className={styles.title}>{t('title')}</h1>
-        <img src={logo} className={styles.logo} alt="logo" />
+        <img src={logo} className={styles.logo} alt={t('altImage') ?? 'logo'} />
         <p>
-          Edit <code>src/sections/hello-dataverse/HelloDataverse.tsx</code> and save to reload.
+          <Trans t={t} i18nKey="description" components={{ 1: <code /> }} />
         </p>
         <a
           className={styles.link}
