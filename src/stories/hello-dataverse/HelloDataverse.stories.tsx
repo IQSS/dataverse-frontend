784f6c7e073f5f57ec368efe69a0b668af358e3b
MellyGray <melinaabril@gmail.com>
2023-02-23 14:45:40 +0100

    fix(files): remove unused stylesheet

--- src/stories/hello-dataverse/HelloDataverse.stories.tsx
+++ src/stories/hello-dataverse/HelloDataverse.stories.tsx
@@ -6,7 +6,6 @@ export default {
   title: 'Hello Dataverse/Page',
   component: HelloDataverse,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen'
   }
 } as ComponentMeta<typeof HelloDataverse>
@@ -17,7 +16,6 @@ export const LoggedOut = Template.bind({})
 
 export const LoggedIn = Template.bind({})
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 LoggedIn.play = ({ canvasElement }) => {
   const canvas = within(canvasElement)
   const loginButton = canvas.getByRole('button', { name: /Log in/i })
