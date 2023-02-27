02920fe61b59ae8f582cba21753ecbd098d47a67
MellyGray <melinaabril@gmail.com>
2023-02-27 12:31:09 +0100

    fix(storybook): add WithLayout decorator

--- src/stories/hello-dataverse/HelloDataverse.stories.tsx
+++ src/stories/hello-dataverse/HelloDataverse.stories.tsx
@@ -1,10 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { within, userEvent } from '@storybook/testing-library'
 import { HelloDataverse } from '../../sections/hello-dataverse/HelloDataverse'
+import { WithLayout } from '../WithLayout'
 
 export default {
   title: 'Hello Dataverse/Page',
   component: HelloDataverse,
+  decorators: [WithLayout],
   parameters: {
     layout: 'fullscreen'
   }
