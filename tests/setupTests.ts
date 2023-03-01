52857e1fb7f3d64ba29bd5958f96405a05a5e355
MellyGray <melinaabril@gmail.com>
2023-03-01 16:34:47 +0100

    feat(Vite): migrate jest to vitest

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -3,8 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
 
-jest.mock('react-i18next', () => ({
+vi.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate hook can use it without a warning being shown
   useTranslation: () => {
     return {
