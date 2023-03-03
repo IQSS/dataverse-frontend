fb52b752341e6f3eb32452b6e9baaf39dd1e266a
MellyGray <melinaabril@gmail.com>
2023-03-03 16:39:23 +0100

    fix(scripts): use same port for serve and preview

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    port: 5173
+  }
 })
