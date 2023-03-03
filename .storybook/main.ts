a628dac39c5fa2639ca2139606a68ebafab0f79e
MellyGray <melinaabril@gmail.com>
2023-03-03 14:05:08 +0100

    fix(Storybook): upgrade a11y addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/react-vite',
