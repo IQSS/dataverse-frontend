6efd78be0d18a8757d9b4034126213f80b53afe2
MellyGray <melinaabril@gmail.com>
2023-03-09 09:23:15 +0100

    feat(Navbar): add dropdown

--- src/sections/ui/navbar/Navbar.tsx
+++ src/sections/ui/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
-import { Navbar as NavbarBS } from 'react-bootstrap'
+import { Navbar as NavbarBS, NavDropdown } from 'react-bootstrap'
 import { Container, Nav } from 'react-bootstrap'
 import './bootstrap-navbar-customized.scss'
 import styles from './Navbar.module.scss'
 
-interface Link {
+export interface Link {
   title: string
-  path: string
+  path: string | Link[]
 }
 
 interface Logo {
@@ -15,7 +15,8 @@ interface Logo {
 
 interface Brand {
   logo: Logo
-  link: Link
+  title: string
+  path: string
 }
 
 interface NavbarProps {
@@ -27,19 +28,32 @@ export function Navbar({ brand, links }: NavbarProps) {
   return (
     <NavbarBS collapseOnSelect expand="lg" fixed="top" className={styles.wrapper}>
       <Container>
-        <NavbarBS.Brand href={brand.link.path}>
+        <NavbarBS.Brand href={brand.path}>
           <img width="28" height="28" src={brand.logo.src} alt={brand.logo.altText ?? 'logo'} />
-          {brand.link.title}
+          {brand.title}
         </NavbarBS.Brand>
         <NavbarBS.Toggle aria-controls="responsive-navbar-nav" />
         <NavbarBS.Collapse id="responsive-navbar-nav" className={styles.collapse}>
           <Nav>
             {links.length != 0 &&
-              links.map((link, index) => (
-                <Nav.Link eventKey={index} key={index} href={link.path}>
-                  {link.title}
-                </Nav.Link>
-              ))}
+              links.map((link, index) =>
+                typeof link.path == 'string' ? (
+                  <Nav.Link eventKey={index} key={index} href={link.path}>
+                    {link.title}
+                  </Nav.Link>
+                ) : (
+                  <NavDropdown key={index} title={link.title} id="basic-nav-dropdown">
+                    {link.path.map(
+                      (link, index) =>
+                        typeof link.path == 'string' && (
+                          <NavDropdown.Item key={index} href={link.path}>
+                            {link.title}
+                          </NavDropdown.Item>
+                        )
+                    )}
+                  </NavDropdown>
+                )
+              )}
           </Nav>
         </NavbarBS.Collapse>
       </Container>
--- src/stories/ui/navbar/Navbar.stories.tsx
+++ src/stories/ui/navbar/Navbar.stories.tsx
@@ -14,10 +14,16 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />
 
 export const Example = Template.bind({})
 Example.args = {
-  brand: { logo: { src: logo, altText: 'logo' }, link: { title: 'Brand Title', path: '#' } },
+  brand: { logo: { src: logo, altText: 'logo' }, title: 'Brand Title', path: '#' },
   links: [
     { title: 'Link 1 ', path: '#' },
     { title: 'Link 2', path: '#' },
-    { title: 'Link 3', path: '#' }
+    {
+      title: 'Dropdown',
+      path: [
+        { title: 'Link 3 ', path: '#' },
+        { title: 'Link 4', path: '#' }
+      ]
+    }
   ]
 }
