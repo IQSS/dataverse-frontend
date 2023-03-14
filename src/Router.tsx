8737b40b39b7d152ca422199937af0fb34b40ed7
GPortas <hey@gportas.me>
2023-03-14 08:49:58 +0000

    Added: builing with base path support and initial pom.xml for .war building and payara deployment

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { HelloDataverse } from './sections/hello-dataverse/HelloDataverse'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HelloDataverse />
-  }
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HelloDataverse />
+    }
+  ],
+  { basename: import.meta.env.BASE_URL }
+)
 
 export function Router() {
   return <RouterProvider router={router} />
