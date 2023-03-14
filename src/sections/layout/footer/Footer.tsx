d51d2d3e2ce7a5c384ff61643dca8fc6f283e4ea
MellyGray <melinaabril@gmail.com>
2023-03-14 09:57:22 +0100

    feat(Footer): add powered by logo

--- src/sections/layout/footer/Footer.tsx
+++ src/sections/layout/footer/Footer.tsx
@@ -2,24 +2,43 @@ import { Container } from '../../ui/grid/container/Container'
 import { Row } from '../../ui/grid/row/Row'
 import { Col } from '../../ui/grid/col/Col'
 import styles from './Footer.module.scss'
+import dataverseProjectLogo from './dataverse-project-logo.svg'
 
 export function Footer() {
   return (
-    <footer>
+    <footer className={styles.container}>
       <Container>
         <Row>
           <Col sm={8}>
             <p className={styles.copyright}>
               Copyright © 2023, The President &amp; Fellows of Harvard College&nbsp;|&nbsp;{' '}
               <a
-                href="http://best-practices.dataverse.org/harvard-policies/harvard-privacy-policy.html"
+                href="https://best-practices.dataverse.org/harvard-policies/harvard-privacy-policy.html"
                 rel="noreferrer"
                 target="_blank">
                 Privacy Policy
               </a>
             </p>
           </Col>
-          <Col sm={4}>2 of 2</Col>
+          <Col sm={4}>
+            <div className={styles['powered-by-container']}>
+              <span className={styles['powered-by-text']}>Powered by</span>
+              <a
+                className={styles['powered-by-logo']}
+                href="https://dataverse.org/"
+                title="The Dataverse Project"
+                target="_blank"
+                rel="noreferrer">
+                <img
+                  src={dataverseProjectLogo}
+                  width="118"
+                  height="40"
+                  alt="The Dataverse Project logo"
+                />
+              </a>
+              <span className={styles.version}>v. 5.13 build 1244-79d6e57</span>
+            </div>
+          </Col>
         </Row>
       </Container>
     </footer>
--- src/stories/layout/footer/Footer.stories.tsx
+++ src/stories/layout/footer/Footer.stories.tsx
@@ -9,6 +9,6 @@ export default {
   }
 } as ComponentMeta<typeof Footer>
 
-const Template: ComponentStory<typeof Footer> = (args) => <Footer />
+const Template: ComponentStory<typeof Footer> = () => <Footer />
 
 export const Default = Template.bind({})
