6adb6d1012ff842a8895704ed61efc6f55bac1ed
MellyGray <melinaabril@gmail.com>
2023-03-16 13:28:02 +0100

    fix: remove unused import

--- tests/sections/layout/header/Header.test.tsx
+++ tests/sections/layout/header/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { Header } from '../../../../src/sections/layout/header/Header'
 
 describe('Header component', () => {
--- tests/sections/ui/navbar/nav-dropdown/NavDropdown.test.tsx
+++ tests/sections/ui/navbar/nav-dropdown/NavDropdown.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { NavDropdown } from '../../../../../src/sections/ui/navbar/nav-dropdown/NavDropdown'
 
 const links = [
