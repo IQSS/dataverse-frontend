5cf08e78894029d501d3bb09629db9fd1ad009e4
MellyGray <melinaabril@gmail.com>
2023-03-16 11:57:18 +0100

    fix(coverage): add Header tests

---
+++ tests/sections/layout/header/Header.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from '../../../../src/sections/layout/header/Header'
+
+describe('Header component', () => {
+  test('displays the user name when the user is logged in', () => {
+    const user = { name: 'John Doe' }
+    const { getByText } = render(<Header user={user} />)
+
+    const userNameElement = getByText('John Doe')
+    expect(userNameElement).toBeInTheDocument()
+  })
+
+  test('displays the Sign Up and Log In links when the user is not logged in', () => {
+    const { getByRole } = render(<Header />)
+
+    const signUpLinkElement = getByRole('link', { name: 'signUp' })
+    expect(signUpLinkElement).toBeInTheDocument()
+
+    const logInLinkElement = getByRole('link', { name: 'logIn' })
+    expect(logInLinkElement).toBeInTheDocument()
+  })
+})
