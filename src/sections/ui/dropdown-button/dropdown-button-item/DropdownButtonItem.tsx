a661c562c4f68c5fb8fc8ee29653e7244a9eea0f
MellyGray <melinaabril@gmail.com>
2023-03-29 14:36:49 +0200

    feat(Dropdown): add dropdown item styles

--- src/sections/ui/dropdown-button/dropdown-button-item/DropdownButtonItem.tsx
+++ src/sections/ui/dropdown-button/dropdown-button-item/DropdownButtonItem.tsx
@@ -1,5 +1,6 @@
 import { Dropdown as DropdownBS } from 'react-bootstrap'
 import { ReactNode } from 'react'
+import styles from './DropdownButtonItem.module.scss'
 
 interface DropdownItemProps {
   href: string
@@ -7,5 +8,9 @@ interface DropdownItemProps {
 }
 
 export function DropdownButtonItem({ href, children }: DropdownItemProps) {
-  return <DropdownBS.Item href={href}>{children}</DropdownBS.Item>
+  return (
+    <DropdownBS.Item className={styles.container} href={href}>
+      {children}
+    </DropdownBS.Item>
+  )
 }
