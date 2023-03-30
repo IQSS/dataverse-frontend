2b89f3f5ffd46175c67d1058dd446b0f9894b381
MellyGray <melinaabril@gmail.com>
2023-03-30 14:51:07 +0200

    fix(Dropdown): increase canvas size of the storybook

--- src/stories/ui/dropdown-button/DropdownButton.stories.tsx
+++ src/stories/ui/dropdown-button/DropdownButton.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { DropdownButton } from '../../../sections/ui/dropdown-button/DropdownButton'
 import { DropdownButtonItem } from '../../../sections/ui/dropdown-button/dropdown-button-item/DropdownButtonItem'
 import { Icon } from '../../../sections/ui/icon.enum'
+import { ReactNode } from 'react'
 
 const meta: Meta<typeof DropdownButton> = {
   title: 'UI/Dropdown Button',
@@ -11,19 +12,25 @@ const meta: Meta<typeof DropdownButton> = {
 export default meta
 type Story = StoryObj<typeof DropdownButton>
 
+const Canvas150Height = ({ children }: { children: ReactNode }) => {
+  return <div style={{ height: 150 }}>{children}</div>
+}
+
 export const Default: Story = {
   render: () => (
-    <DropdownButton withSpacing title="Dropdown Button" id="dropdown-1">
-      <DropdownButtonItem href="/item-1">Item 1</DropdownButtonItem>
-      <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
-      <DropdownButtonItem href="/item-3">Item 3</DropdownButtonItem>
-    </DropdownButton>
+    <Canvas150Height>
+      <DropdownButton withSpacing title="Dropdown Button" id="dropdown-1">
+        <DropdownButtonItem href="/item-1">Item 1</DropdownButtonItem>
+        <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
+        <DropdownButtonItem href="/item-3">Item 3</DropdownButtonItem>
+      </DropdownButton>
+    </Canvas150Height>
   )
 }
 
 export const Variants: Story = {
   render: () => (
-    <>
+    <Canvas150Height>
       <DropdownButton withSpacing title="Primary" id="dropdown-1" variant="primary">
         <DropdownButtonItem href="/item-1">Item 1</DropdownButtonItem>
         <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
@@ -39,21 +46,23 @@ export const Variants: Story = {
         <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
         <DropdownButtonItem href="/item-3">Item 3</DropdownButtonItem>
       </DropdownButton>
-    </>
+    </Canvas150Height>
   )
 }
 
 export const WithIcon: Story = {
   render: () => (
-    <DropdownButton
-      withSpacing
-      title="Dropdown Button"
-      id="dropdown-1"
-      variant="primary"
-      icon={Icon.DATAVERSE}>
-      <DropdownButtonItem href="/item-1">Item 1</DropdownButtonItem>
-      <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
-      <DropdownButtonItem href="/item-3">Item 3</DropdownButtonItem>
-    </DropdownButton>
+    <Canvas150Height>
+      <DropdownButton
+        withSpacing
+        title="Dropdown Button"
+        id="dropdown-1"
+        variant="primary"
+        icon={Icon.DATAVERSE}>
+        <DropdownButtonItem href="/item-1">Item 1</DropdownButtonItem>
+        <DropdownButtonItem href="/item-2">Item 2</DropdownButtonItem>
+        <DropdownButtonItem href="/item-3">Item 3</DropdownButtonItem>
+      </DropdownButton>
+    </Canvas150Height>
   )
 }
