b22e0446950cb85e5e3475fcad13a7baebdf68cb
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-02 18:52:06 -0400

    add another story with interaction test on LoginForm

--- src/stories/login-form/LoginForm.stories.tsx
+++ src/stories/login-form/LoginForm.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { LoginForm } from '../../sections/login-form/LoginForm'
 import { userEvent, within } from '@storybook/testing-library'
-import { expect } from '@storybook/jest'
+import { expect, jest } from '@storybook/jest'
 const meta: Meta<typeof LoginForm> = {
   title: 'Pages/Login Form',
   component: LoginForm
@@ -10,15 +10,13 @@ const meta: Meta<typeof LoginForm> = {
 export default meta
 type Story = StoryObj<typeof LoginForm>
 
+// @ts-ignore
+const loginFn = ({ username, password }) => {
+  console.log(`Logging in with username: ${username} and password: ${password}`)
+}
+const jestSpy = jest.fn()
 export const Basic: Story = {
-  render: () => (
-    <LoginForm
-      onLogin={({ username, password }) =>
-        console.log(`Logging in with username: ${username} and password: ${password}`)
-      }
-      title="Log In"
-    />
-  )
+  render: () => <LoginForm onLogin={loginFn} title="Log In" />
 }
 
 export const BasicPlusInteraction: Story = {
@@ -38,5 +36,17 @@ export const BasicPlusInteraction: Story = {
     userEvent.click(canvas.getByTestId('login-test'))
     expect(canvas.getByTestId('login-test')).toBeInTheDocument()
     expect(canvas.getByText('Login')).toBeInTheDocument()
+  }
+}
+export const SubmitTest: Story = {
+  render: () => <LoginForm onLogin={jestSpy} title="Log In" />,
+
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.type(canvas.getByTestId('username'), 'johndoe')
+    await userEvent.type(canvas.getByTestId('password'), 'password')
+    await userEvent.click(canvas.getByTestId('submitButton'))
+    expect(jestSpy).toHaveBeenCalledWith({ username: 'johndoe', password: 'password' })
   }
 }
