92267212dc989f714b46cea1fadcedeedb182b9d
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-03 07:58:08 -0400

    added scripts for running Cypress storybook tests separately

---
+++ cypress.storybook.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'cypress'
+import vitePreprocessor from 'cypress-vite'
+import path from 'path'
+
+export default defineConfig({
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:6006',
+    specPattern: 'tests/e2e/storybook/**/*.spec.{js,jsx,ts,tsx}',
+    screenshotOnRunFailure: false,
+    video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    supportFile: 'tests/e2e/support/e2e.ts',
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor(path.resolve(__dirname, './vite.config.ts')))
+    }
+  },
+  component: {
+    specPattern: 'tests/component/tests/**/*.spec.{js,jsx,ts,tsx}',
+    supportFile: 'tests/component/support/component.ts',
+    devServer: {
+      framework: 'react',
+      bundler: 'vite'
+    }
+  }
+})
--- tests/e2e/tests/hello-dataverse.spec.ts
+++ tests/e2e/tests/hello-dataverse.spec.ts
@@ -4,12 +4,3 @@ describe('Hello Dataverse', () => {
     cy.findAllByText(/Hello Dataverse/i).should('exist')
   })
 })
-
-describe('Button Story Test', () => {
-  it('Should display the button', () => {
-    Cypress.config('baseUrl', 'http://localhost:6006')
-    cy.visit('/iframe.html?id=ui-button--primary')
-    cy.pause()
-    cy.get('[data-testid="button-test"]').should('exist')
-  })
-})
