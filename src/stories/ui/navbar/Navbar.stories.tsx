81ca5ac96d049b5db5162ba69dc9e8e4403de387
MellyGray <melinaabril@gmail.com>
2023-04-04 14:46:58 +0200

    fix(Navbar): add docs with mdx template

--- src/stories/ui/navbar/Navbar.stories.tsx
+++ src/stories/ui/navbar/Navbar.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { WithI18next } from '../../WithI18next'
 import { Navbar } from '../../../sections/ui/navbar/Navbar'
 import logo from '../../../sections/ui/logo.svg'
+import { CanvasFixedHeight } from '../CanvasFixedHeight'
 
 const meta: Meta<typeof Navbar> = {
   title: 'UI/Navbar',
@@ -12,28 +13,44 @@ const meta: Meta<typeof Navbar> = {
 export default meta
 type Story = StoryObj<typeof Navbar>
 
-export const Example: Story = {
+export const Default: Story = {
   render: () => (
-    <Navbar
-      brand={{ logo: { src: logo, altText: 'logo' }, title: 'Brand Title', path: '#' }}
-      links={[
-        { title: 'Link 1 ', value: '#' },
-        { title: 'Link 2', value: '#' },
-        {
-          title: 'Dropdown 1',
-          value: [
-            { title: 'Link 3 ', value: '#' },
-            { title: 'Link 4', value: '#' }
-          ]
-        },
-        {
-          title: 'Dropdown 2',
-          value: [
-            { title: 'Link 5 ', value: '#' },
-            { title: 'Link 6', value: '#' }
-          ]
-        }
-      ]}
-    />
+    <CanvasFixedHeight height={150}>
+      <Navbar
+        brand={{ logo: { src: logo, altText: 'logo' }, title: 'Brand Title', path: '#' }}
+        links={[
+          { title: 'Link 1 ', value: '#' },
+          { title: 'Link 2', value: '#' },
+          {
+            title: 'Dropdown 1',
+            value: [
+              { title: 'Link 3 ', value: '#' },
+              { title: 'Link 4', value: '#' }
+            ]
+          },
+          {
+            title: 'Dropdown 2',
+            value: [
+              { title: 'Link 5 ', value: '#' },
+              { title: 'Link 6', value: '#' }
+            ]
+          }
+        ]}
+      />
+    </CanvasFixedHeight>
+  )
+}
+
+export const Menu: Story = {
+  render: () => (
+    <CanvasFixedHeight height={150}>
+      <Navbar
+        brand={{ logo: { src: logo, altText: 'logo' }, title: 'Dataverse', path: '#' }}
+        links={[
+          { title: 'Sign Up', value: '#' },
+          { title: 'Log In', value: '#' }
+        ]}
+      />
+    </CanvasFixedHeight>
   )
 }
