eb1c9eba7593702595d87cb1c1bc85b02880ac4e
MellyGray <melinaabril@gmail.com>
2023-04-05 16:15:12 +0200

    feat(DesignTokens): create the baseTheme from SASS variables

---
+++ src/sections/ui/theme/BaseTheme.ts
@@ -0,0 +1,9 @@
+import variables from '../../../assets/styles/design-tokens/colors.module.scss'
+
+export const baseTheme = {
+  themeKey: 'base',
+  color: {
+    primary: variables.brandColor,
+    secondary: variables.secondaryColor
+  }
+}
