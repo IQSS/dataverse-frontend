14145854ef287b6a496f9707781ca160248d768a
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-12 10:44:55 -0400

    add HelloDataverse component test

---
+++ cypress/component/hello-dataverse/helloDataverse.spec.tsx
@@ -0,0 +1,14 @@
+import { HelloDataverse } from '../../../src/sections/hello-dataverse/HelloDataverse'
+import { I18nextProvider } from 'react-i18next'
+import i18next from '../../../src/i18n'
+
+describe('HelloDataverse page', () => {
+  it('renders hello dataverse title', () => {
+    cy.mount(
+      <I18nextProvider i18n={i18next}>
+        <HelloDataverse />
+      </I18nextProvider>
+    )
+    cy.get('h2').should('contain.text', 'Hello Dataverse')
+  })
+})
