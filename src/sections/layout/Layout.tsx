8da59fb766306f06b399637e1ee3f909ae69eba2
MellyGray <melinaabril@gmail.com>
2023-04-12 12:29:15 +0200

    fix(folders): remove unnecessary folders

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './header/Header'
-import { Container } from '../ui/grid/container/Container'
+import { Container } from '../ui/grid/Container'
 import { Footer } from './footer/Footer'
 import styles from './Layout.module.scss'
 
--- src/sections/ui/navbar/Navbar.tsx
+++ src/sections/ui/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Navbar as NavbarBS } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
 import { Link, NavbarProps } from './NavbarProps'
 import { NavDropdown } from './nav-dropdown/NavDropdown'
-import { Container } from '../grid/container/Container'
+import { Container } from '../grid/Container'
 
 export function Navbar({ brand, links }: NavbarProps) {
   return (
