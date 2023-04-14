c7e12a32624ad9bec48f218eb6e92ca22f81aae9
MellyGray <melinaabril@gmail.com>
2023-04-14 12:32:12 +0200

    feat(Breadcrumb): add tests

---
+++ tests/sections/ui/breadcrumb/Breadcrumb.test.tsx
@@ -0,0 +1,19 @@
+import { Breadcrumb } from '../../../../src/sections/ui/breadcrumb/Breadcrumb'
+import { render } from '@testing-library/react'
+
+describe('Breadcrumb', () => {
+  test('renders without errors when given valid props', () => {
+    const { getByRole } = render(<Breadcrumb>Test</Breadcrumb>)
+    expect(getByRole('navigation')).toBeInTheDocument()
+  })
+
+  test('renders the children prop', () => {
+    const { getByText } = render(<Breadcrumb>Test</Breadcrumb>)
+    expect(getByText('Test')).toBeInTheDocument()
+  })
+
+  test('has the correct role', () => {
+    const { getByRole } = render(<Breadcrumb>Test</Breadcrumb>)
+    expect(getByRole('navigation')).toBeInTheDocument()
+  })
+})
