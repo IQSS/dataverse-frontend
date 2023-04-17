75ce2b4982543f79ca02375f5161bfb468ef2795
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-17 16:03:36 -0400

    add all placement options to story

--- src/stories/ui/tooltip/Tooltip.stories.tsx
+++ src/stories/ui/tooltip/Tooltip.stories.tsx
@@ -12,3 +12,15 @@ type Story = StoryObj<typeof Tooltip>
 export const Default: Story = {
   render: () => <Tooltip placement="right" message="Tooltip on right."></Tooltip>
 }
+
+export const AllVariantsAtAGlance: Story = {
+  render: () => (
+    <>
+      <Tooltip placement="right" message="Tooltip on right."></Tooltip>
+      <Tooltip placement="bottom" message="Tooltip on bottom."></Tooltip>
+      <Tooltip placement="top" message="Tooltip on top."></Tooltip>
+      <Tooltip placement="auto" message="Tooltip auto placement."></Tooltip>
+      <Tooltip placement="left" message="Tooltip on left."></Tooltip>
+    </>
+  )
+}
