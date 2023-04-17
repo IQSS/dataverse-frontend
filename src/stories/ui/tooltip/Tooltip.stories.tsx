4be9f07f6bf6ac23442e9a4c400c6970fa9ecfee
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-17 15:33:53 -0400

    moved styles into scss module

--- src/stories/ui/tooltip/Tooltip.stories.tsx
+++ src/stories/ui/tooltip/Tooltip.stories.tsx
@@ -12,12 +12,3 @@ type Story = StoryObj<typeof Tooltip>
 export const Default: Story = {
   render: () => <Tooltip placement="right" message="Tooltip on right."></Tooltip>
 }
-
-export const AllVariantsAtAGlance: Story = {
-  render: () => (
-    <>
-      <Tooltip placement="right" message="Tooltip on right."></Tooltip>
-      <Tooltip placement="left" message="Tooltip on left."></Tooltip>
-    </>
-  )
-}
