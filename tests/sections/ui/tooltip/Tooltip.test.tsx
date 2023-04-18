5618eab51d6c6be60e15ba184f331883071bba6e
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-18 10:57:23 -0400

    replaced questionmark SVG with React component

--- tests/sections/ui/tooltip/Tooltip.test.tsx
+++ tests/sections/ui/tooltip/Tooltip.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { Tooltip, TooltipProps } from '../../../../src/sections/ui/tooltip/Tooltip'
 
 describe('Tooltip', () => {
@@ -10,4 +10,19 @@ describe('Tooltip', () => {
   it('renders without crashing', () => {
     render(<Tooltip {...defaultProps} />)
   })
+  it('renders without crashing', () => {
+    const { container, findByText } = render(<Tooltip {...defaultProps} />)
+    const svg = container.querySelector('span > svg')
+    expect(svg).toBeInTheDocument()
+    fireEvent.mouseOver(svg)
+    //expect(findByText('Tooltip')).toBeInTheDocument()
+  })
+  /*
+  it('try example without crashing', async () => {
+    const { container, findByText } = render(<Tooltip {...defaultProps} />)
+    const svg = container.querySelector('span > svg')
+    expect(svg).toBeInTheDocument()
+    fireEvent.mouseOver(svg)
+    expect(await findByText('Tooltip').toBeInTheDocument())
+  })*/
 })
