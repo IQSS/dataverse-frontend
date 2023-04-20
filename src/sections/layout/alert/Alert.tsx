2babeebdb35512be7c7150e02c30c67e89b70918
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-04-20 15:56:14 -0400

    remove comment

--- src/sections/layout/alert/Alert.tsx
+++ src/sections/layout/alert/Alert.tsx
@@ -1,53 +1,3 @@
-/*
-export function Alert({ variant, dismissible = true, children, customHeading }: AlertProps) {
-  interface AlertIcons {
-    [key: string]: JSX.Element
-  }
-  interface AlertHeadings {
-    [key: string]: string
-  }
-  const ALERT_ICONS: AlertIcons = {
-    success: <CheckCircleFill></CheckCircleFill>,
-    info: <InfoCircleFill></InfoCircleFill>,
-    warning: <ExclamationTriangleFill></ExclamationTriangleFill>,
-    danger: <ExclamationCircleFill></ExclamationCircleFill>
-  }
-
-  const ALERT_HEADINGS: AlertHeadings = {
-    success: 'Success!',
-    info: 'Information',
-    warning: 'Warning',
-    danger: 'Error'
-  }
-  const [show, setShow] = useState(true)
-
-  function getAlertIcon(variant: AlertVariant): JSX.Element {
-    return ALERT_ICONS[variant]
-  }
-
-  function getVariantHeading(variant: AlertVariant, customHeading?: string): string {
-    return customHeading ?? ALERT_HEADINGS[variant];
-  }
-
-  const heading = getVariantHeading(variant, customHeading);
-
-  return (
-      <>
-        {show && (
-            <AlertBS variant={variant} onClose={() => setShow(false)} dismissible={dismissible}>
-          <span role="img" aria-label={`alert-icon-${variant}`}>
-            {getAlertIcon(variant)}
-          </span>{' '}
-              &nbsp;
-              {heading}
-              {children}
-            </AlertBS>
-        )}
-      </>
-  )
-}
-*/
-
 import { Alert as AlertBS } from 'react-bootstrap'
 import { ReactNode, useState } from 'react'
 import {
@@ -62,8 +12,8 @@ type AlertVariant = 'success' | 'info' | 'warning' | 'danger'
 interface AlertProps {
   variant: AlertVariant
   dismissible?: boolean
-  children: ReactNode
   customHeading?: string
+  children: ReactNode
 }
 
 export function Alert({ variant, dismissible = true, customHeading, children }: AlertProps) {
