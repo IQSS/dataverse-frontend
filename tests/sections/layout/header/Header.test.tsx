15838557b164d427052e588c61001409a3ed142f
GPortas <hey@gportas.me>
2023-04-24 12:41:16 +0100

    Added: missing tests

--- tests/sections/layout/header/Header.test.tsx
+++ tests/sections/layout/header/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { SinonSandbox, createSandbox } from 'sinon'
 import { Header } from '../../../../src/sections/layout/header/Header'
 import { createAuthenticatedUser } from '../../../testHelpers/users/authenticatedUserHelper'
@@ -7,7 +7,6 @@ import { createLogout } from '../../../testHelpers/auth/logoutHelper'
 
 describe('Header component', () => {
   const sandbox: SinonSandbox = createSandbox()
-  const logoutMock = createLogout()
 
   afterEach(() => {
     sandbox.restore()
@@ -17,6 +16,7 @@ describe('Header component', () => {
     const testAuthenticatedUser = createAuthenticatedUser()
     const getCurrentAuthenticatedUserStub = createGetCurrentAuthenticatedUser()
     getCurrentAuthenticatedUserStub.execute = sandbox.stub().resolves(testAuthenticatedUser)
+    const logoutMock = createLogout()
     const { findByText } = render(
       <Header getCurrentAuthenticatedUser={getCurrentAuthenticatedUserStub} logout={logoutMock} />
     )
@@ -28,6 +28,7 @@ describe('Header component', () => {
   test('displays the Sign Up and Log In links when the user is not logged in', () => {
     const getCurrentAuthenticatedUserStub = createGetCurrentAuthenticatedUser()
     getCurrentAuthenticatedUserStub.execute = sandbox.stub().rejects({})
+    const logoutMock = createLogout()
     const { getByRole } = render(
       <Header getCurrentAuthenticatedUser={getCurrentAuthenticatedUserStub} logout={logoutMock} />
     )
@@ -36,6 +37,29 @@ describe('Header component', () => {
     expect(signUpLinkElement).toBeInTheDocument()
 
     const logInLinkElement = getByRole('link', { name: 'logIn' })
+    expect(logInLinkElement).toBeInTheDocument()
+  })
+
+  test('displays the Sign Up and Log In links after user Log Out', async () => {
+    const testAuthenticatedUser = createAuthenticatedUser()
+    const getCurrentAuthenticatedUserStub = createGetCurrentAuthenticatedUser()
+    getCurrentAuthenticatedUserStub.execute = sandbox.stub().resolves(testAuthenticatedUser)
+    const logoutStub = createLogout()
+    logoutStub.execute = sandbox.stub().resolves({})
+    const { findByText, findByRole } = render(
+      <Header getCurrentAuthenticatedUser={getCurrentAuthenticatedUserStub} logout={logoutStub} />
+    )
+
+    const userNameElement = await findByText('Test User')
+    fireEvent.click(userNameElement)
+
+    const logOutLinkElement = await findByRole('button', { name: 'logOut' })
+    fireEvent.click(logOutLinkElement)
+
+    const signUpLinkElement = await findByRole('link', { name: 'signUp' })
+    expect(signUpLinkElement).toBeInTheDocument()
+
+    const logInLinkElement = await findByRole('link', { name: 'logIn' })
     expect(logInLinkElement).toBeInTheDocument()
   })
 })
--- tests/sections/ui/dropdown-button/DropdownButton.test.tsx
+++ tests/sections/ui/dropdown-button/DropdownButton.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render } from '@testing-library/react'
 import { DropdownButton } from '../../../../src/sections/ui/dropdown-button/DropdownButton'
 import { Icon } from '../../../../src/sections/ui/icon.enum'
+import styles from '../../../../src/sections/ui/dropdown-button/DropdownButton.module.scss'
 
 const titleText = 'My Dropdown Button'
 
@@ -47,5 +48,17 @@ describe('DropdownButton', () => {
       </DropdownButton>
     )
     expect(getByRole('img', { name: Icon.COLLECTION })).toBeInTheDocument()
+  })
+
+  it('renders with spacing attributes', () => {
+    const { getByText } = render(
+      <DropdownButton id="my-dropdown" title="My Dropdown Button" variant="primary" withSpacing>
+        <span>Item 1</span>
+        <span>Item 2</span>
+      </DropdownButton>
+    )
+
+    const dropdownButton = getByText(titleText)
+    expect(dropdownButton.parentElement).toHaveClass(styles.spacing)
   })
 })
