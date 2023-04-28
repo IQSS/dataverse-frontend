1c9d52543876fe50e2c81ea904eabea76b39fddb
GPortas <hey@gportas.me>
2023-04-28 12:50:26 +0100

    Added: redirectPage query param to JSF LOG_IN route

--- src/sections/route.enum.ts
+++ src/sections/route.enum.ts
@@ -1,6 +1,6 @@
 export enum Route {
   HOME = '/',
   SIGN_UP = '/dataverseuser.xhtml?editMode=CREATE',
-  LOG_IN = '/loginpage.xhtml',
+  LOG_IN = '/loginpage.xhtml?redirectPage=%2Fdataverse.xhtml',
   LOG_OUT = '/'
 }
