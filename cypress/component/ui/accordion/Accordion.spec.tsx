1f1ec6be4d1cdb97378ef1980a570787fdf53f51
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-05-01 10:29:49 -0400

    added more tests to Accordion

--- cypress/component/ui/accordion/Accordion.spec.tsx
+++ cypress/component/ui/accordion/Accordion.spec.tsx
@@ -1,29 +1,82 @@
 import { Accordion } from '../../../../src/sections/ui/accordion/Accordion'
+import { ThemeProvider } from '../../../../src/sections/ui/theme/ThemeProvider'
+
+//TODO: remove ThemeProvider and replace with customMount()
 
 describe('Accordion', () => {
+  const section1Header = 'Section 1'
+  const section1Body = 'Content 1'
+  const section2Header = 'Section 2'
+  const section2Body = 'Content 2'
   it('renders an accordion with defaultActiveKey prop and children', () => {
     cy.mount(
-      <Accordion defaultActiveKey="1">
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Section 1</Accordion.Header>
-          <Accordion.Body>Content 1</Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>Section 2</Accordion.Header>
-          <Accordion.Body>Content 2</Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+      <ThemeProvider>
+        <Accordion defaultActiveKey="1">
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>{section1Header}</Accordion.Header>
+            <Accordion.Body>{section1Body}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>{section2Header}</Accordion.Header>
+            <Accordion.Body>{section2Body}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </ThemeProvider>
     )
+
+    cy.findByText(section1Header).should('be.visible')
+    cy.findByText(section1Body).should('be.visible')
+    cy.findByText(section2Header).should('be.visible')
+    cy.findByText(section2Body).should('not.be.visible')
+
+    cy.findByText(section1Header).click()
+    cy.findByText(section1Body).should('not.be.visible')
+  })
+  it('renders without a defaultActiveKey', () => {
+    cy.mount(
+      <ThemeProvider>
+        <Accordion alwaysOpen={true}>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>{section1Header}</Accordion.Header>
+            <Accordion.Body>{section1Body}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>{section2Header}</Accordion.Header>
+            <Accordion.Body>{section2Body}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </ThemeProvider>
+    )
+    cy.findByText(section1Header).should('be.visible')
+    cy.findByText(section1Body).should('not.be.visible')
+    cy.findByText(section2Header).should('be.visible')
+    cy.findByText(section2Body).should('not.be.visible')
+
     cy.pause()
-    const section1Header = cy.findByText('Section 1')
-    const section1Body = cy.findByText('Content 1')
+  })
+  it('renders the always open tab correctly', () => {
+    cy.mount(
+      <ThemeProvider>
+        <Accordion alwaysOpen={true}>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>{section1Header}</Accordion.Header>
+            <Accordion.Body>{section1Body}</Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>{section2Header}</Accordion.Header>
+            <Accordion.Body>{section2Body}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </ThemeProvider>
+    )
+    cy.findByText(section1Header).should('be.visible')
+    cy.findByText(section1Body).should('not.be.visible')
+    cy.findByText(section2Header).should('be.visible')
+    cy.findByText(section2Body).should('not.be.visible')
 
-    const section2Header = cy.findByText('Section 2')
-    const section2Body = cy.findByText('Content 2')
-
-    section1Header.should('be.visible')
-    section1Body.should('exist')
-    section2Header.should('be.visible')
-    section2Body.should('exist')
+    cy.findByText(section2Header).click()
+    cy.findByText(section1Header).click()
+    cy.findByText(section1Body).should('be.visible')
+    cy.findByText(section2Body).should('be.visible')
   })
 })
