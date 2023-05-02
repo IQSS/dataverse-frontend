5ccd29beb8b8bad81c931f7a950834d2e66783e9
GPortas <hey@gportas.me>
2023-05-02 17:53:50 +0100

    Added: sample data configuration to run-env

--- dev-env/run-env.sh
+++ dev-env/run-env.sh
@@ -2,6 +2,8 @@
 
 export DATAVERSE_BRANCH_NAME=$1
 
+DATAVERSE_HOST=http://localhost:8080
+
 echo "INFO - Setting up Dataverse on branch ${DATAVERSE_BRANCH_NAME}..."
 
 echo "INFO - Cloning Dataverse backend repository..."
@@ -14,7 +16,7 @@ echo "INFO - Waiting for containers to be ready..."
 # Up to ~5 minutes
 max_attempts=30
 n_attempts=0
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/api/info/version); do
+until $(curl --output /dev/null --silent --head --fail ${DATAVERSE_HOST}/api/info/version); do
     if [ ${n_attempts} -eq ${max_attempts} ];then
       echo "ERROR - Timeout reached while waiting for containers to be ready"
       ./rm-env.sh
@@ -28,8 +30,28 @@ done
 echo "INFO - Bootstrapping dataverse..."
 cd dataverse
 ./scripts/dev/docker-final-setup.sh
-echo "INFO - Bootstrapping finished!"
 
 echo "INFO - Cleaning up repository..."
 cd ..
 rm -rf dataverse
+
+echo "INFO - Cloning Dataverse sample data repository..."
+git clone git@github.com:IQSS/dataverse-sample-data.git
+
+echo "INFO - Configuring Dataverse sample data repository..."
+cd dataverse-sample-data
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cp ../dvconfig.py ./dvconfig.py
+curl -X PUT -d 'true' ${DATAVERSE_HOST}/api/admin/settings/:AllowApiTokenLookupViaApi
+dataverse_api_token=$(python3 get_api_token.py)
+sed -i '' "s/<DATAVERSE_HOST>/${DATAVERSE_HOST}/g" dvconfig.py
+sed -i '' "s/<DATAVERSE_API_TOKEN>/${dataverse_api_token}/g" dvconfig.py
+
+echo "INFO - Creating sample data..."
+python3 create_sample_data.py
+
+echo "INFO - Cleaning up repository..."
+cd ..
+rm -rf dataverse-sample-data
