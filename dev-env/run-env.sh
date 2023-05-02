836b4375807f11f26c49f59332857d445cf36b52
GPortas <hey@gportas.me>
2023-05-02 15:56:20 +0100

    Added: initial dev environment based on docker-compose and bash scripts

---
+++ dev-env/run-env.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+export DATAVERSE_BRANCH_NAME=$1
+
+echo "INFO - Setting up Dataverse on branch ${DATAVERSE_BRANCH_NAME}..."
+
+echo "INFO - Cloning Dataverse backend repository..."
+git clone -b ${DATAVERSE_BRANCH_NAME} git@github.com:IQSS/dataverse.git
+
+echo "INFO - Running docker containers..."
+docker-compose -f "./docker-compose-dev.yml" up -d --build
+
+echo "INFO - Waiting for containers to be ready..."
+# Up to ~5 minutes
+max_attempts=30
+n_attempts=0
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/api/info/version); do
+    if [ ${n_attempts} -eq ${max_attempts} ];then
+      echo "ERROR - Timeout reached while waiting for containers to be ready"
+      ./rm-env.sh
+      rm -rf dataverse
+      exit 1
+    fi
+    n_attempts=$(($n_attempts+1))
+    sleep 10
+done
+
+echo "INFO - Bootstrapping dataverse..."
+cd dataverse
+./scripts/dev/docker-final-setup.sh
+echo "INFO - Bootstrapping finished!"
+
+echo "INFO - Cleaning up repository..."
+cd ..
+rm -rf dataverse
