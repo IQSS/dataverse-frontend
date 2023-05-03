53f1a9a8eee0cf9749205cb2a95e959694cda29b
MellyGray <melinaabril@gmail.com>
2023-05-03 13:15:51 +0200

    fix: cy component tests for the version number

--- cypress/component/layout/Layout.spec.tsx
+++ cypress/component/layout/Layout.spec.tsx
@@ -1,27 +1,42 @@
+import { createSandbox, SinonSandbox } from 'sinon'
+import { HeaderFactory } from '../../../src/sections/layout/header/HeaderFactory'
+import { FooterFactory } from '../../../src/sections/layout/footer/FooterFactory'
+import { FooterMother } from '../../../tests/sections/layout/footer/FooterMother'
+import { HeaderMother } from '../../../tests/sections/layout/header/HeaderMother'
 import { Layout } from '../../../src/sections/layout/Layout'
 
 describe('Layout', () => {
-  it('renders the Header', () => {
-    cy.mount(<Layout></Layout>)
+  const sandbox: SinonSandbox = createSandbox()
 
-    cy.findByRole('img', { name: 'Brand Logo Image' }).should('exist')
-    cy.findByText('brandTitle').should('exist')
-
-    cy.findByRole('button', { name: 'Toggle navigation' }).click()
-    cy.findByRole('link', { name: 'signUp' }).should('exist')
-    cy.findByRole('link', { name: 'logIn' }).should('exist')
+  afterEach(() => {
+    sandbox.restore()
+    sandbox.stub(HeaderFactory, 'create').returns(HeaderMother.withLoggedInUser(sandbox))
+    sandbox.stub(FooterFactory, 'create').returns(FooterMother.withDataverseVersion(sandbox))
   })
-  it('renders the Footer', () => {
-    cy.mount(<Layout></Layout>)
 
-    it('displays the Powered By link', () => {
-      cy.mount(<Layout></Layout>)
-      cy.findByRole('link', { name: 'The Dataverse Project logo' }).should('exist')
-    })
+  it('renders the header', () => {
+    cy.mount(<Layout />)
 
-    it('displays the Privacy Policy', () => {
-      cy.mount(<Layout></Layout>)
-      cy.findByText('privacyPolicy').should('exist')
-    })
+    const brandLink = cy.findByRole('link', { name: 'Brand Logo Image brandTitle' })
+    brandLink.should('exist')
+
+    const brandImg = cy.findByRole('img', { name: 'Brand Logo Image' })
+    brandImg.should('exist')
+  })
+
+  it('renders the footer', () => {
+    cy.mount(<Layout />)
+
+    const copyright = cy.findByText('copyright')
+    copyright.should('exist')
+
+    const privacyPolicy = cy.findByRole('link', { name: 'privacyPolicy' })
+    privacyPolicy.should('exist')
+
+    const poweredByLink = cy.findByRole('link', { name: 'The Dataverse Project logo' })
+    poweredByLink.should('exist')
+
+    const poweredByImg = cy.findByRole('img', { name: 'The Dataverse Project logo' })
+    poweredByImg.should('exist')
   })
 })
--- cypress/component/layout/footer/Footer.spec.tsx
+++ cypress/component/layout/footer/Footer.spec.tsx
@@ -1,13 +1,36 @@
 import { Footer } from '../../../../src/sections/layout/footer/Footer'
+import { createSandbox, SinonSandbox } from 'sinon'
+import { DataverseVersionMother } from '../../../../tests/info/models/DataverseVersionMother'
+import { FooterMother } from '../../../../tests/sections/layout/footer/FooterMother'
+import { DataverseInfoRepository } from '../../../../src/info/domain/repositories/DataverseInfoRepository'
 
 describe('Footer component', () => {
-  it('displays the Powered By link', () => {
-    cy.mount(<Footer />)
-    cy.findByRole('link', { name: 'The Dataverse Project logo' }).should('exist')
+  const sandbox: SinonSandbox = createSandbox()
+  const testVersion = DataverseVersionMother.create()
+
+  it('should render footer content', () => {
+    cy.mount(FooterMother.withDataverseVersion(sandbox, testVersion))
+
+    cy.findByText('copyright').should('exist')
+    cy.findByText('privacyPolicy').should('exist')
+    cy.findByText('poweredBy').should('exist')
+    cy.findByAltText('The Dataverse Project logo').should('exist')
+    cy.findByText(testVersion).should('exist')
   })
 
-  it('displays the Privacy Policy', () => {
-    cy.mount(<Footer />)
+  it('should call dataverseInfoRepository.getVersion on mount', () => {
+    const dataverseInfoRepository: DataverseInfoRepository = {
+      getVersion: cy.stub().resolves(testVersion)
+    }
+
+    cy.mount(<Footer dataverseInfoRepository={dataverseInfoRepository} />)
+
+    cy.wrap(dataverseInfoRepository.getVersion()).should('have.been.called')
+  })
+
+  it('should open privacy policy link in new tab', () => {
+    cy.mount(FooterMother.withDataverseVersion(sandbox))
+
     cy.findByText('privacyPolicy').should('exist')
   })
 })
