3ef2748384c63364a9416ef27e5d6739fa0b213c
MellyGray <melinaabril@gmail.com>
2023-05-09 15:52:01 +0200

    fix(e2e tests): add Api initialization before running the tests

--- cypress/e2e/info/infrastructure/repositories/DataverseInfoJSDataverseRepository.spec.ts
+++ cypress/e2e/info/infrastructure/repositories/DataverseInfoJSDataverseRepository.spec.ts
@@ -1,12 +1,16 @@
 import { DataverseInfoJSDataverseRepository } from '../../../../../src/info/infrastructure/repositories/DataverseInfoJSDataverseRepository'
 import chai from 'chai'
 import chaiAsPromised from 'chai-as-promised'
+import { ApiConfig } from 'js-dataverse/dist/core'
 
 chai.use(chaiAsPromised)
 
 const expect = chai.expect
 
 describe('DataverseInfo JSDataverse Repository', () => {
+  const VITE_DATAVERSE_BACKEND_URL = (import.meta.env.VITE_DATAVERSE_BACKEND_URL as string) ?? ''
+  ApiConfig.init(`${VITE_DATAVERSE_BACKEND_URL}/api/v1`)
+
   it('gets the dataverse version number', async () => {
     const dataverseInfoRepository = new DataverseInfoJSDataverseRepository()
     const dataverseVersion = await dataverseInfoRepository.getVersion()
