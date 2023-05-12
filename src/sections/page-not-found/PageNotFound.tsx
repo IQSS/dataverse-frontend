e01b97ebf24ab53a2180477485f058e88718927b
MellyGray <melinaabril@gmail.com>
2023-05-12 13:45:47 +0200

    feat(PageNotFound): use the Alert component

--- src/sections/page-not-found/PageNotFound.tsx
+++ src/sections/page-not-found/PageNotFound.tsx
@@ -1,13 +1,10 @@
-// TODO: Substitute this with the Alert component of the design system
+import { Alert } from 'dataverse-design-system'
 
 export function PageNotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>
-        The page you are looking for was not found. If you believe this is an error, please contact
-        Demo Dataverse Support for assistance.
-      </p>
-    </div>
+    <Alert variant="danger" customHeading="Page Not Found" dismissible={false}>
+      The page you are looking for was not found. If you believe this is an error, please contact
+      Demo Dataverse Support for assistance.
+    </Alert>
   )
 }
