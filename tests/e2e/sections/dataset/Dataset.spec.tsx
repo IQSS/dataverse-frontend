5291c383641e0f82dbdc21a663eb82d17b343bf2
MellyGray <melinaabril@gmail.com>
2023-05-16 16:30:19 +0200

    fix(e2e): missing configuration details

--- tests/e2e/sections/dataset/Dataset.spec.tsx
+++ tests/e2e/sections/dataset/Dataset.spec.tsx
@@ -1,16 +1,10 @@
 describe('Dataset', () => {
   it('successfully loads a dataset when passing the id', () => {
-    cy.visit('/dataset/12345')
+    cy.visit('/datasets/12345')
 
     cy.findByRole('heading', { name: 'Dataset Title' }).should('exist')
     cy.findByText('Version 1.0').should('exist')
     cy.findByText('Draft').should('exist')
-  })
-
-  it('renders Hello Dataverse when no id is provided', () => {
-    cy.visit('/dataset')
-
-    cy.findAllByText(/Hello Dataverse/i).should('exist')
   })
 
   // TODO - Add test for when the dataset is not found and loading skeleton when the js-dataverse module is ready
