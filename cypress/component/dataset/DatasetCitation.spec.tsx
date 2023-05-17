aad22ee19e00983d3e448520c6aedbd9412d8bf2
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-05-17 11:02:31 -0400

    update Cypress component test

--- cypress/component/dataset/DatasetCitation.spec.tsx
+++ cypress/component/dataset/DatasetCitation.spec.tsx
@@ -1,8 +1,8 @@
 import { createSandbox, SinonSandbox } from 'sinon'
-import { DatasetRepository } from '../../../src/dataset/domain/repositories/DatasetRepository'
 import { DatasetMother } from '../../../tests/dataset/domain/models/DatasetMother'
 import { DatasetCitation } from '../../../src/sections/dataset/datasetCitation/DatasetCitation'
 import { ThemeProvider } from '../../../src/sections/ui/theme/ThemeProvider'
+import { Citation } from '../../../src/dataset/domain/models/Dataset'
 
 describe('DatasetCitation', () => {
   const sandbox: SinonSandbox = createSandbox()
@@ -13,14 +13,37 @@ describe('DatasetCitation', () => {
   })
 
   it('renders the DatasetCitation fields', () => {
-    const datasetRepository: DatasetRepository = {} as DatasetRepository
-    datasetRepository.getById = sandbox.stub().resolves(testDataset)
+    const citationFields: Citation = {
+      authors: ['Bennet, Elizabeth', 'Darcy, Fitzwilliam'],
+      title: 'Test Terms',
+      creationYear: 2023,
+      persistentIdentifier: 'https://doi.org/10.70122/FK2/KLX4XO',
+      persistentIdentifierUrl: 'https://doi.org/10.70122/FK2/KLX4XO',
+      publisher: 'Demo Dataverse',
+      version: 'V1'
+    }
     // TODO: remove ThemeProvider and replace with customMount()
     cy.mount(
       <ThemeProvider>
-        <DatasetCitation datasetRepository={datasetRepository} id={testDataset.id} />
+        <DatasetCitation citation={citationFields} />
       </ThemeProvider>
     )
-    cy.findByText('Dropdown Citation').should('exist')
+    citationFields.authors.map((author) => {
+      cy.findByText(new RegExp(`${author}`)).should('exist')
+    })
+    cy.findByText('Data Citation Standards.').should('exist')
+    cy.findByText(new RegExp(`${citationFields.title}`)).should('exist')
+    cy.findByText(new RegExp(`${citationFields.creationYear}`)).should('exist')
+    cy.findByText(new RegExp(`${citationFields.publisher}`)).should('exist')
+    cy.findByText(new RegExp(`${citationFields.version}`)).should('exist')
+    cy.findByRole('link', { name: citationFields.persistentIdentifier })
+      .should('have.attr', 'href')
+      .and('eq', citationFields.persistentIdentifierUrl)
+
+    cy.findByRole('link', { name: 'Data Citation Standards.' })
+      .should('have.attr', 'href')
+      .and('eq', 'https://dataverse.org/best-practices/data-citation')
+    cy.findByRole('article').should('exist')
+    cy.findByText(/DEACCESSIONED VERSION/).should('not.exist')
   })
 })
