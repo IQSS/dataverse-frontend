1447983e73b6396275b249f99d2f79e7c1d8c8a0
MellyGray <melinaabril@gmail.com>
2023-05-25 16:04:14 +0200

    feat(jsDataverseInvoke): get dataset title from module response

--- src/dataset/infrastructure/mappers/DatasetMapper.ts
+++ src/dataset/infrastructure/mappers/DatasetMapper.ts
@@ -1,14 +1,20 @@
-import { Dataset as JSDataset } from '@IQSS/dataverse-client-javascript'
+import {
+  Dataset as JSDataset,
+  DatasetMetadataBlock as JSDatasetMetadataBlock
+} from '@IQSS/dataverse-client-javascript'
 import { DatasetVersionState as JSDatasetVersionState } from '@IQSS/dataverse-client-javascript/dist/datasets/domain/models/Dataset'
-import { DatasetCitation, Dataset, DatasetStatus } from '../../domain/models/Dataset'
+import {
+  DatasetCitation,
+  Dataset,
+  DatasetStatus,
+  MetadataBlockName
+} from '../../domain/models/Dataset'
 
 export class DatasetMapper {
   static toModel(jsDataset: JSDataset): Dataset {
-    DatasetMapper.validateJsDataset(jsDataset)
-
     return new Dataset.Builder(
       jsDataset.persistentId,
-      jsDataset.metadataBlocks[0].fields.title as string,
+      DatasetMapper.toTitle(jsDataset.metadataBlocks),
       jsDataset.versionInfo,
       DatasetMapper.toStatus(jsDataset.versionInfo.state),
       DatasetMapper.toCitation(),
@@ -16,12 +22,6 @@ export class DatasetMapper {
       jsDataset.license,
       []
     )
-  }
-
-  static validateJsDataset(jsDataset: JSDataset): void {
-    if (typeof jsDataset.metadataBlocks[0].fields.title !== 'string') {
-      throw new Error('Dataset title is not a string')
-    }
   }
 
   static toStatus(jsDatasetVersionState: JSDatasetVersionState): DatasetStatus {
@@ -37,11 +37,47 @@ export class DatasetMapper {
     }
   }
 
+  static toTitle(jsDatasetMetadataBlocks: JSDatasetMetadataBlock[]): string {
+    const citationFields = jsDatasetMetadataBlocks.find(
+      (metadataBlock) => metadataBlock.name === MetadataBlockName.CITATION
+    )?.fields
+
+    if (citationFields && typeof citationFields.title === 'string') {
+      return citationFields.title
+    }
+
+    throw new Error('Dataset title not found')
+  }
+
   static toCitation(): DatasetCitation {
+    // TODO: Implement
     return {
       citationText: '',
       url: '',
       publisher: ''
     }
   }
+
+  // static toMetadataBlocks(
+  //   jsDatasetMetadataBlocks: JSDatasetMetadataBlock[]
+  // ): DatasetMetadataBlock[] {
+  //   return jsDatasetMetadataBlocks.map((jsDatasetMetadataBlock) => {
+  //     return {
+  //       name: DatasetMapper.toMetadataBlockName(jsDatasetMetadataBlock.name),
+  //       fields: DatasetMapper.toMetadataBlockName(jsDatasetMetadataBlock.name)
+  //     }
+  //   })
+  // }
+  //
+  // static toMetadataBlockName(jsDatasetMetadataBlockName: string): MetadataBlockName {
+  //   const metadataBlockNameKey = Object.values(MetadataBlockName).find((metadataBlockNameKey) => {
+  //     return metadataBlockNameKey === jsDatasetMetadataBlockName
+  //   })
+  //
+  //   if (metadataBlockNameKey === undefined) {
+  //     throw new Error('Incorrect Metadata block name key')
+  //   }
+  //
+  //   return metadataBlockNameKey
+  // }
 }
