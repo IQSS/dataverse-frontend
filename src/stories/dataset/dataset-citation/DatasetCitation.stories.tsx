25edbf6e9f3b2d84ef15181c33da5af99a0183fc
MellyGray <melinaabril@gmail.com>
2023-05-29 13:24:33 +0200

    fix(DatasetCitation story): remove unhandled state

--- src/stories/dataset/dataset-citation/DatasetCitation.stories.tsx
+++ src/stories/dataset/dataset-citation/DatasetCitation.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { WithI18next } from '../WithI18next'
-import { DatasetCitation } from '../../sections/dataset/dataset-citation/DatasetCitation'
-import { DatasetStatus, DatasetVersion } from '../../dataset/domain/models/Dataset'
-import { DatasetMockData } from './DatasetMockData'
+import { WithI18next } from '../../WithI18next'
+import { DatasetCitation } from '../../../sections/dataset/dataset-citation/DatasetCitation'
+import { DatasetStatus, DatasetVersion } from '../../../dataset/domain/models/Dataset'
+import { DatasetMockData } from '../DatasetMockData'
 
 const meta: Meta<typeof DatasetCitation> = {
   title: 'Sections/Dataset Page/DatasetCitation',
@@ -21,21 +21,6 @@ export const Default: Story = {
         <br></br>
         <br></br>
         <DatasetCitation citation={dataset.citation} version={dataset.version} />
-      </div>
-    )
-  }
-}
-export const WithUNF: Story = {
-  render: () => {
-    const dataset = DatasetMockData({
-      citation:
-        'Admin, Dataverse, 2023, "Dataset Title", <a href="https://doi.org/10.5072/FK2/BUDNRV" target="_blank">https://doi.org/10.5072/FK2/BUDNRV</a>, Root, V1, UNF:6:8ttuxucTZJWfZ9JgN1udiA== [fileUNF]'
-    })
-
-    return (
-      <div>
-        <br></br>
-        <br></br> <DatasetCitation citation={dataset.citation} version={dataset.version} />
       </div>
     )
   }
