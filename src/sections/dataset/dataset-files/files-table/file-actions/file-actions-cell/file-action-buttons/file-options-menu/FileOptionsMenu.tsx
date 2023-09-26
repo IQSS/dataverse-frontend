f41efadb21fbfb34dc904e4ca738b95a89fb4cc6
MellyGray <melinaabril@gmail.com>
2023-09-26 10:50:47 +0200

    feat(FileOptionsMenu): add dataset permissions

--- src/sections/dataset/dataset-files/files-table/file-actions/file-actions-cell/file-action-buttons/file-options-menu/FileOptionsMenu.tsx
+++ src/sections/dataset/dataset-files/files-table/file-actions/file-actions-cell/file-action-buttons/file-options-menu/FileOptionsMenu.tsx
@@ -7,16 +7,15 @@ import { useTranslation } from 'react-i18next'
 import { useFileEditDatasetPermission } from '../../../../../../../file/file-permissions/useFileEditDatasetPermission'
 import { useState } from 'react'
 import { FileAlreadyDeletedModal } from './FileAlreadyDeletedModal'
+import { useDataset } from '../../../../../../DatasetContext'
 
 export function FileOptionsMenu({ file }: { file: File }) {
   const { t } = useTranslation('files')
   const { user } = useSession()
-  const { sessionUserHasEditDatasetPermission } = useFileEditDatasetPermission(file)
-  const datasetHasValidTermsOfAccess = true // TODO - Implement terms of access validation
-  const datasetLockedFromEdits = false // TODO - Ask Guillermo if this a dataset property coming from the api
+  const { dataset } = useDataset()
   const [showFileAlreadyDeletedModal, setShowFileAlreadyDeletedModal] = useState(false)
 
-  if (!user || !sessionUserHasEditDatasetPermission || !datasetHasValidTermsOfAccess) {
+  if (!user || !dataset?.permissions.canUpdateDataset || !dataset.hasValidTermsOfAccess) {
     return <></>
   }
 
@@ -26,7 +25,7 @@ export function FileOptionsMenu({ file }: { file: File }) {
         <Tooltip placement="top" overlay={<span>{t('actions.optionsMenu.title')}</span>}>
           <Button
             id={`file-options-file-${file.id}`}
-            disabled={datasetLockedFromEdits}
+            disabled={dataset.isLockedFromEdits}
             variant="secondary"
             icon={
               <ThreeDotsVertical
@@ -49,7 +48,7 @@ export function FileOptionsMenu({ file }: { file: File }) {
       <DropdownButton
         id={`file-options-file-${file.id}`}
         title=""
-        disabled={datasetLockedFromEdits}
+        disabled={dataset.isLockedFromEdits}
         asButtonGroup
         variant="secondary"
         icon={<ThreeDotsVertical aria-label={t('actions.optionsMenu.title')} />}>
--- src/stories/dataset/WithDatasetLockedFromEdits.tsx
+++ src/stories/dataset/WithDatasetLockedFromEdits.tsx
@@ -1,13 +1,14 @@
 import { StoryFn } from '@storybook/react'
-import { DatasetProvider } from '../../sections/dataset/DatasetProvider'
 import { DatasetRepository } from '../../dataset/domain/repositories/DatasetRepository'
 import { Dataset } from '../../dataset/domain/models/Dataset'
 import {
+  DatasetLockMother,
   DatasetMother,
   DatasetPermissionsMother
 } from '../../../tests/component/dataset/domain/models/DatasetMother'
+import { DatasetProvider } from '../../sections/dataset/DatasetProvider'
 
-export const WithDatasetAllPermissionsGranted = (Story: StoryFn) => {
+export const WithDatasetLockedFromEdits = (Story: StoryFn) => {
   const datasetRepository = {} as DatasetRepository
   datasetRepository.getByPersistentId = (
     persistentId: string,
@@ -17,8 +18,9 @@ export const WithDatasetAllPermissionsGranted = (Story: StoryFn) => {
       setTimeout(() => {
         resolve(
           DatasetMother.createRealistic({
+            persistentId: persistentId,
             permissions: DatasetPermissionsMother.createWithAllAllowed(),
-            hasValidTermsOfAccess: true
+            locks: [DatasetLockMother.createLockedInEditInProgress()]
           })
         )
       }, 1000)
