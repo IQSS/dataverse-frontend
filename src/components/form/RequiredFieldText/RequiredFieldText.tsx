bdc6e93e3697f48d5fbd7e5c7618100eb1c0b129
Matt Mangan <matt@mangan.io>
2024-01-05 14:06:35 -0500

    updated RequiredFieldText to use translation

--- src/components/form/RequiredFieldText/RequiredFieldText.tsx
+++ src/components/form/RequiredFieldText/RequiredFieldText.tsx
@@ -1,10 +1,12 @@
 import { RequiredInputSymbol } from '@iqss/dataverse-design-system'
+import { useTranslation } from 'react-i18next'
 
 export function RequiredFieldText() {
+  const { t } = useTranslation('createDataset')
   return (
     <p>
       <RequiredInputSymbol />
-      Asterisks indicate required fields
+      {t('requiredFields')}
     </p>
   )
 }
