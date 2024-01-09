c50a2b35a7890b22a6a561f67240f61ff761a8c0
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-01-09 17:24:23 -0500

    fix: DatasetPreview mapper and mock data

--- src/dataset/infrastructure/mappers/JSDatasetPreviewMapper.ts
+++ src/dataset/infrastructure/mappers/JSDatasetPreviewMapper.ts
@@ -15,7 +15,7 @@ export class JSDatasetPreviewMapper {
         jsDatasetPreview.versionId,
         jsDatasetPreview.versionInfo
       ),
-      jsDatasetPreview.versionInfo.releaseTime !== undefined,
+      jsDatasetPreview.versionInfo.releaseTime == undefined ? true : false,
       jsDatasetPreview.citation,
       jsDatasetPreview.versionInfo.state === JSDatasetVersionState.DEACCESSIONED,
       JSDatasetPreviewMapper.toPreviewDate(jsDatasetPreview.versionInfo),
