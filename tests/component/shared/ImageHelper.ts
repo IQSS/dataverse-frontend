578522ad14cbe31f56d178c224a43895b60550a6
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-02-23 22:21:26 -0500

    fix: use picsum image, which is static

--- tests/component/shared/ImageHelper.ts
+++ tests/component/shared/ImageHelper.ts
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker'
 export function getImageUrl(width?: number, height?: number) {
   // Always use a static image for chromatic builds
   if (import.meta.env.STORYBOOK_CHROMATIC_BUILD === 'true') {
-    return 'https://loremflickr.com/320/240?lock=1'
+    return 'https://picsum.photos/id/237/200'
   } else {
     faker.image.imageUrl(width, height)
   }
