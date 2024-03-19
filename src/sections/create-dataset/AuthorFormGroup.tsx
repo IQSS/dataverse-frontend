98027382ab999f0a1b63801d7612fc1856d15806
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-03-19 19:08:31 -0400

    feat: add DynamicFieldsButtons.tsx

--- src/sections/create-dataset/AuthorFormGroup.tsx
+++ src/sections/create-dataset/AuthorFormGroup.tsx
@@ -6,12 +6,12 @@ import { DatasetMetadataSubField } from '../../dataset/domain/models/Dataset'
 import { DatasetDTO } from '../../dataset/domain/useCases/DTOs/DatasetDTO'
 import { useDatasetFormData } from './useDatasetFormData'
 import { useDatasetValidator } from './useDatasetValidator'
-
+import { Col, Row } from '@iqss/dataverse-design-system'
+import { DynamicFieldsButtons } from './dynamic-fields-buttons/DynamicFieldsButtons'
 interface AuthorFormGroupProps {
   submissionStatus: SubmissionStatus
-  handleFieldChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  handleFieldChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   validationErrors: DatasetDTO
-
 }
 
 export function AuthorFormGroup({
@@ -22,28 +22,42 @@ export function AuthorFormGroup({
   const { t } = useTranslation('createDataset')
   const { datasetIsValid } = useDatasetValidator()
   const { formData, updateFormData } = useDatasetFormData(datasetIsValid)
+  const index = 0
 
   console.log(JSON.stringify(validationErrors))
-  const index = 0
+
   return (
     <Form.Group controlId="author-name" required>
-      <Form.Group.Label message={t('datasetForm.fields.authorName.tooltip')}>
-        {t('datasetForm.fields.authorName.label')}
-      </Form.Group.Label>
-      <Form.Group.Input
-        disabled={submissionStatus === SubmissionStatus.IsSubmitting}
-        type="text"
-        name={`metadataBlocks.0.fields.author.${index}.authorName`}
-        onChange={handleFieldChange}
-        isInvalid={
-          !!(validationErrors.metadataBlocks[index].fields.author as DatasetMetadataSubField[])[
-            index
-          ].authorName
-        }
-      />
-      <Form.Group.Feedback type="invalid">
-        {t('datasetForm.fields.authorName.feedback')}
-      </Form.Group.Feedback>
+      <Row key={index}>
+        <Col sm={3}>
+          <Form.Group.Label message={t('datasetForm.fields.authorName.tooltip')}>
+            {t('datasetForm.fields.authorName.label')}
+          </Form.Group.Label>
+        </Col>
+        <Col sm={6}>
+          <Form.Group.Input
+            disabled={submissionStatus === SubmissionStatus.IsSubmitting}
+            type="text"
+            name={`metadataBlocks.0.fields.author.${index}.authorName`}
+            onChange={handleFieldChange}
+            isInvalid={
+              !!(validationErrors.metadataBlocks[index].fields.author as DatasetMetadataSubField[])[
+                index
+              ].authorName
+            }
+          />
+          <Form.Group.Feedback type="invalid">
+            {t('datasetForm.fields.authorName.feedback')}
+          </Form.Group.Feedback>
+        </Col>
+        <Col sm={3}>
+          <DynamicFieldsButtons
+            originalField={index === 0}
+            onAddButtonClick={() => updateFormData('metadataBlocks.0.fields.author', '')}
+            onRemoveButtonClick={() => updateFormData('metadataBlocks.0.fields.author', '')}
+          />
+        </Col>
+      </Row>
     </Form.Group>
   )
 }
