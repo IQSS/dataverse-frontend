548459eab401c4a112a0a3b628ebc2a0c042c8f2
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-04-04 16:26:29 -0400

    refactor: (useDatasets.tsx) use getDatasetsWithCount() to get paged datasets

--- src/sections/collection/datasets-list/useDatasets.tsx
+++ src/sections/collection/datasets-list/useDatasets.tsx
@@ -19,78 +19,28 @@ export function useDatasets(
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [totalDatasetsCount, setTotalDatasetsCount] = useState<TotalDatasetsCount>()
 
-  const fetchDatasets = (totalDatasetsCount: TotalDatasetsCount) => {
-    if (typeof totalDatasetsCount !== 'undefined') {
-      if (totalDatasetsCount === 0) {
-        setIsLoading(false)
-        return
-      }
-      if (paginationInfo.page > paginationInfo.totalPages) {
-        setPageNumberNotFound(true)
-        setIsLoading(false)
-        return
-      }
-      return getDatasets(datasetRepository, collectionId, paginationInfo)
-        .then((datasets: DatasetPreview[]) => {
-          setDatasets(datasets)
-          setIsLoading(false)
-          return datasets
-        })
-        .catch(() => {
-          throw new Error('There was an error getting the datasets')
-        })
-    }
-  }
-  const fetchTotalDatasetsCount: () => Promise<TotalDatasetsCount> = () => {
-    return getTotalDatasetsCount(datasetRepository, collectionId)
-      .then((totalDatasetsCount: TotalDatasetsCount) => {
-        setTotalDatasetsCount(totalDatasetsCount)
-        if (totalDatasetsCount !== paginationInfo.totalItems) {
-          paginationInfo = paginationInfo.withTotal(totalDatasetsCount)
-          onPaginationInfoChange(paginationInfo)
-        }
-        return totalDatasetsCount
-      })
-      .catch(() => {
-        throw new Error('There was an error getting the datasets count info')
-      })
-  }
-
   const fetchDatasetsWithCount = () => {
-    console.log('fetching datasets with count')
-    console.log('collectionId', collectionId)
-    console.log('paginationInfo', paginationInfo)
     return getDatasetsWithCount(datasetRepository, collectionId, paginationInfo)
       .then((datasetsWithCount: DatasetsWithCount) => {
-        console.log('datasetsWithCount', datasetsWithCount)
-        setDatasets(datasetsWithCount.datasetPreviews)
-        setTotalDatasetsCount(datasetsWithCount.totalCount)
-        if (typeof datasetsWithCount.totalCount !== 'undefined') {
-          if (datasetsWithCount.totalCount === 0) {
-            console.log('no datasets found')
-            setIsLoading(false)
-            return Promise.resolve()
-          }
-        }
-        if (datasetsWithCount.totalCount !== paginationInfo.totalItems) {
-          paginationInfo = paginationInfo.withTotal(datasetsWithCount.totalCount)
-          onPaginationInfoChange(paginationInfo)
-        }
-        console.log('paginationInfo', paginationInfo)
-        console.log('totalPages', paginationInfo.totalPages)
-        if (paginationInfo.page > paginationInfo.totalPages) {
-          setPageNumberNotFound(true)
+        setTotalDatasetsCount(totalDatasetsCount)
+        if (datasetsWithCount.totalCount === 0) {
           setIsLoading(false)
           return Promise.resolve()
         }
-        if (datasetsWithCount.totalCount !== paginationInfo.totalItems) {
+        if (totalDatasetsCount !== paginationInfo.totalItems) {
           paginationInfo = paginationInfo.withTotal(datasetsWithCount.totalCount)
           onPaginationInfoChange(paginationInfo)
+
+          if (paginationInfo.page > paginationInfo.totalPages) {
+            setPageNumberNotFound(true)
+            setIsLoading(false)
+            return Promise.resolve()
+          }
+          setDatasets(datasetsWithCount.datasetPreviews)
+          setIsLoading(false)
         }
-        setIsLoading(false)
       })
       .catch((error) => {
-        console.log(JSON.stringify(error))
         throw new Error('There was an error getting the datasets')
       })
   }
