a6ab73734637be99e27b1c3b5a7a5811bb46a491
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-04-26 14:46:56 -0400

    feat: first draft of upload form

--- src/sections/upload-dataset-files/UploadDatasetFiles.tsx
+++ src/sections/upload-dataset-files/UploadDatasetFiles.tsx
@@ -4,6 +4,10 @@ import { useLoading } from '../loading/LoadingContext'
 import { useDataset } from '../dataset/DatasetContext'
 import { PageNotFound } from '../page-not-found/PageNotFound'
 import { BreadcrumbsGenerator } from '../shared/hierarchy/BreadcrumbsGenerator'
+import { Accordion, Form, Tabs } from '@iqss/dataverse-design-system'
+import styles from '../dataset/Dataset.module.scss'
+import { useTranslation } from 'react-i18next'
+import { useForm } from 'react-hook-form'
 
 interface UploadDatasetFilesProps {
   fileRepository: FileRepository
@@ -14,7 +18,8 @@ export const UploadDatasetFiles = ({
 }: UploadDatasetFilesProps) => {
   const { setIsLoading } = useLoading()
   const { dataset, isLoading } = useDataset()
-
+  const { t } = useTranslation('uploadDatasetFiles')
+  const form = useForm({ mode: 'onChange' })
   useEffect(() => {
     setIsLoading(isLoading)
   }, [isLoading])
@@ -31,7 +36,26 @@ export const UploadDatasetFiles = ({
         <>
           <BreadcrumbsGenerator hierarchy={dataset.hierarchy} />
           <article>
-            <p>Metadata Files uploading goes here</p>
+            <Tabs defaultActiveKey="files">
+              <Tabs.Tab eventKey="files" title={t('filesTabTitle')}>
+                <div className={styles['tab-container']}>
+                  {t('fileTypeSupport')}
+                  <Accordion defaultActiveKey="upload">
+                    <Accordion.Item eventKey="upload" key="uploadKey">
+                      <Accordion.Header>{t('accordionHeader')}</Accordion.Header>
+                      <Accordion.Body>
+                        <Form>
+                          <Form.Group controlId="basic-form-username">
+                            <Form.Group.Label>Select Files to Add</Form.Group.Label>
+                            <Form.Group.Input type="file" placeholder="File" />
+                          </Form.Group>
+                        </Form>
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </div>
+              </Tabs.Tab>
+            </Tabs>
           </article>
         </>
       )}
