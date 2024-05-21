9e65bf720e2ad27e4ad058f43ade4cce8792a311
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-05-21 08:54:51 -0400

    fix: flex styles

--- src/sections/create-dataset/HostCollectionForm/HostCollectionForm.tsx
+++ src/sections/create-dataset/HostCollectionForm/HostCollectionForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from '@iqss/dataverse-design-system'
+import { Button, Col } from '@iqss/dataverse-design-system'
 import { Form } from '@iqss/dataverse-design-system'
 import { useTranslation } from 'react-i18next'
 import { useNotImplementedModal } from '../../not-implemented/NotImplementedModalContext'
@@ -18,18 +18,16 @@ export function HostCollectionForm({ collectionId }: HostCollectionFormProps) {
         <Form.Group.Label message={t('hostCollection.description')} column sm={3} required>
           {t('hostCollection.label')}
         </Form.Group.Label>
-        <Col sm={9}>
-          <Row className={styles['input-button-wrapper']}>
-            <Col sm={6}>
-              {t('hostCollection.helpText')}
-              <Form.Group.Input type="text" disabled defaultValue={collectionId} />
-            </Col>
-            <Col className={styles['edit-button-wrapper']} sm={3}>
-              <Button type={'button'} onClick={showModal} variant="secondary">
-                {t('hostCollection.buttonLabel')}
-              </Button>
-            </Col>
-          </Row>
+        <Col sm={9} className={styles['input-button-wrapper']}>
+          <Col md={9}>
+            <Form.Group.Text>{t('hostCollection.helpText')}</Form.Group.Text>
+            <Form.Group.Input type="text" disabled defaultValue={collectionId} />
+          </Col>
+          <Col className={styles['edit-button-wrapper']} md={3}>
+            <Button type={'button'} onClick={showModal} variant="secondary">
+              {t('hostCollection.buttonLabel')}
+            </Button>
+          </Col>
         </Col>
       </Form.Group>
     </>
