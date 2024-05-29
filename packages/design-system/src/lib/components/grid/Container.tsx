9aceb28d01ec3e311cdbb978bcedd984c7da0833
Germán Saracca <gersaracca@gmail.com>
2024-05-29 08:47:09 -0300

    feat(design system): Container ability to forward ref

--- packages/design-system/src/lib/components/grid/Container.tsx
+++ packages/design-system/src/lib/components/grid/Container.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, forwardRef, RefObject } from 'react'
 import { Container as ContainerBS } from 'react-bootstrap'
 import * as React from 'react'
 
@@ -6,6 +6,17 @@ export interface ContainerProps extends React.HTMLAttributes<HTMLElement> {
   children: ReactNode
 }
 
-export function Container({ children, ...props }: ContainerProps) {
-  return <ContainerBS {...props}>{children}</ContainerBS>
-}
+export const Container = forwardRef(
+  (
+    { children, ...props }: ContainerProps,
+    ref: RefObject<HTMLDivElement> | ((instance: HTMLDivElement | null) => void) | null | undefined
+  ) => {
+    return (
+      <ContainerBS {...props} ref={ref}>
+        {children}
+      </ContainerBS>
+    )
+  }
+)
+
+Container.displayName = 'Container'
