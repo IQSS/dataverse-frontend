a20c1a13681f7717a847f528390bbdbcda277941
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-05-29 13:16:29 -0400

    replace component with a custom hook

--- src/sections/dataset/Dataset.tsx
+++ src/sections/dataset/Dataset.tsx
@@ -20,7 +20,7 @@ import { SeparationLine } from '../shared/layout/SeparationLine/SeparationLine'
 import { BreadcrumbsGenerator } from '../shared/hierarchy/BreadcrumbsGenerator'
 import { useAlertContext } from '../alerts/AlertContext'
 import { AlertMessageKey } from '../../alert/domain/models/Alert'
-import { DatasetJsonLd } from './dataset-json-ld/DatasetJsonLd'
+import { useAddDatasetJsonLd } from './useAddDatasetJsonLd'
 
 interface DatasetProps {
   fileRepository: FileRepository
@@ -33,6 +33,7 @@ export function Dataset({ fileRepository, created }: DatasetProps) {
   const { t } = useTranslation('dataset')
   const { hideModal, isModalOpen } = useNotImplementedModal()
   const { addDatasetAlert } = useAlertContext()
+  useAddDatasetJsonLd({ persistentId: dataset?.persistentId })
 
   if (created) {
     addDatasetAlert({ messageKey: AlertMessageKey.DATASET_CREATED, variant: 'success' })
@@ -47,7 +48,6 @@ export function Dataset({ fileRepository, created }: DatasetProps) {
 
   return (
     <>
-      <DatasetJsonLd persistentId={dataset?.persistentId}></DatasetJsonLd>
       <NotImplementedModal show={isModalOpen} handleClose={hideModal} />
       {!dataset ? (
         <PageNotFound />
