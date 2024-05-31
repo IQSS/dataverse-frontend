7b026c12110648094b935acc678747d6c962d06a
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-05-31 16:30:05 +0200

    accessibility fix by custom progress bar

--- packages/design-system/src/lib/components/progress-bar/ProgressBar.tsx
+++ packages/design-system/src/lib/components/progress-bar/ProgressBar.tsx
@@ -1,9 +1,24 @@
-import { ProgressBar as ProgressBarBS } from 'react-bootstrap'
-
 interface ProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
   now?: number
 }
 
-export function ProgressBar({ now }: ProgressBarProps) {
-  return <ProgressBarBS now={now} />
+export function ProgressBar({
+  now,
+  'aria-label': ariaLabel = 'progress bar',
+  ...rest
+}: ProgressBarProps) {
+  return (
+    <div className="progress">
+      <div
+        role="progressbar"
+        className="progress-bar"
+        style={{ width: now ? now.toString() + '%' : undefined }}
+        aria-valuenow={now}
+        aria-valuemin={0}
+        aria-valuemax={100}
+        aria-label={ariaLabel}
+        {...rest}
+      />
+    </div>
+  )
 }
