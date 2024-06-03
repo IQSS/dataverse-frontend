6b7689a8df66f59c612214a1a8d8a42e4a9e4d66
Germán Saracca <gersaracca@gmail.com>
2024-06-03 10:38:17 -0300

    fix: unneeded target remove entries

--- src/shared/hooks/useObserveElementSize.ts
+++ src/shared/hooks/useObserveElementSize.ts
@@ -4,22 +4,17 @@ export const useObserveElementSize = (elementRef: React.RefObject<HTMLElement>) 
   const [size, setSize] = useState({ width: 0, height: 0 })
 
   useEffect(() => {
-    let resizeObserverEntries: ResizeObserverEntry[] = []
-
     const observer = new ResizeObserver((entries) => {
       const target = entries[0].target
       const width = target.clientWidth
       const height = target.clientHeight
 
       setSize({ width, height })
-
-      resizeObserverEntries = entries
     })
 
     if (elementRef.current) observer.observe(elementRef.current)
 
     return () => {
-      resizeObserverEntries.forEach((entry) => entry.target.remove())
       observer.disconnect()
     }
   }, [elementRef])
