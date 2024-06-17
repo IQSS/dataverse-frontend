735dba95290da950aff3a8cf8f12fbc4ab5a87db
Ellen Kraffmiller <ekraffmiller@g.harvard.edu>
2024-06-17 15:07:50 -0400

    Fix: use DRAFT version DatasetCardHeader search param, if it exists

--- src/sections/collection/datasets-list/dataset-card/DatasetCardHeader.tsx
+++ src/sections/collection/datasets-list/dataset-card/DatasetCardHeader.tsx
@@ -3,17 +3,27 @@ import { LinkToPage } from '../../../shared/link-to-page/LinkToPage'
 import { Route } from '../../../Route.enum'
 import { DatasetLabels } from '../../../dataset/dataset-labels/DatasetLabels'
 import { DatasetIcon } from '../../../dataset/dataset-icon/DatasetIcon'
-import { DatasetVersion } from '../../../../dataset/domain/models/Dataset'
+import { DatasetPublishingStatus, DatasetVersion } from '../../../../dataset/domain/models/Dataset'
 
 interface DatasetCardHeaderProps {
   persistentId: string
   version: DatasetVersion
 }
+function getVersionSearchParam(version: DatasetVersion): string {
+  return version.publishingStatus == DatasetPublishingStatus.DRAFT
+    ? 'DRAFT'
+    : version.number.toString()
+}
 export function DatasetCardHeader({ persistentId, version }: DatasetCardHeaderProps) {
   return (
     <div className={styles.header}>
       <div className={styles.title}>
-        <LinkToPage page={Route.DATASETS} searchParams={{ persistentId: persistentId }}>
+        <LinkToPage
+          page={Route.DATASETS}
+          searchParams={{
+            persistentId: persistentId,
+            version: getVersionSearchParam(version)
+          }}>
           {version.title}
         </LinkToPage>
         <DatasetLabels labels={version.labels} />
