110b9a6b1f5c7274186f4e1e1f4fbeba9269393e
Ellen Kraffmiller <ekraffmiller@g.harvard.edu>
2024-06-28 09:36:20 -0400

    add CC0 link to help text

--- src/sections/dataset/publish-dataset/PublishDatasetHelpText.tsx
+++ src/sections/dataset/publish-dataset/PublishDatasetHelpText.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 
 interface PublishDatasetHelpTextProps {
   releasedVersionExists: boolean
@@ -6,7 +7,7 @@ interface PublishDatasetHelpTextProps {
 
 export function PublishDatasetHelpText({ releasedVersionExists }: PublishDatasetHelpTextProps) {
   const { t } = useTranslation('publishDataset')
-
+  const cc0Link = 'https://creativecommons.org/publicdomain/zero/1.0/'
   return (
     <>
       {!releasedVersionExists && <p>{t('draftQuestion')}</p>}
@@ -18,7 +19,12 @@ export function PublishDatasetHelpText({ releasedVersionExists }: PublishDataset
           margin: '10px',
           backgroundColor: 'rgb(220, 220, 220)'
         }}>
-        <p>{t('termsText1')}</p>
+        <Trans
+          t={t}
+          i18nKey="termsText1"
+          values={{ cc0Link }}
+          components={{ a: <a href={cc0Link} /> }}
+        />
         <p>{t('termsText2')}</p>
       </div>
     </>
