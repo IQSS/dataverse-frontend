83c9b49876d5ba24caf01cd59a2746412f9ca5ef
Germán Saracca <gersaracca@gmail.com>
2024-07-12 12:43:51 -0300

    feat: created state and alert

--- src/sections/new-collection/NewCollectionFactory.tsx
+++ src/sections/new-collection/NewCollectionFactory.tsx
@@ -20,6 +20,7 @@ function NewCollectionWithParams() {
     <NewCollection
       ownerCollectionId={ownerCollectionId}
       collectionRepository={collectionRepository}
+      key={ownerCollectionId}
     />
   )
 }
