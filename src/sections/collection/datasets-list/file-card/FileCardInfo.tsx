25278c011c4bf982da73cc7fd9be6ca6b1967640
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-08-07 14:34:05 -0400

    fix: move functions to FileCardHelper.ts

--- src/sections/collection/datasets-list/file-card/FileCardInfo.tsx
+++ src/sections/collection/datasets-list/file-card/FileCardInfo.tsx
@@ -4,24 +4,13 @@ import { FilePreview } from '../../../../files/domain/models/FilePreview'
 import { Stack } from '@iqss/dataverse-design-system'
 import { LinkToPage } from '../../../shared/link-to-page/LinkToPage'
 import { Route } from '../../../Route.enum'
-import { DatasetPublishingStatus } from '../../../../dataset/domain/models/Dataset'
 import { FileChecksum } from '../../../dataset/dataset-files/files-table/file-info/file-info-cell/file-info-data/FileChecksum'
 import { FileTabularData } from '../../../dataset/dataset-files/files-table/file-info/file-info-cell/file-info-data/FileTabularData'
+import { FileCardHelper } from './FileCardHelper'
 
 interface FileCardInfoProps {
   filePreview: FilePreview
   persistentId: string
-}
-function getSearchParams(
-  persistentId: string,
-  publishingStatus: DatasetPublishingStatus
-): Record<string, string> {
-  const params: Record<string, string> = { persistentId: persistentId }
-  if (publishingStatus === DatasetPublishingStatus.DRAFT) {
-    // TODO: Replace with const after merge of #442
-    params.version = 'DRAFT'
-  }
-  return params
 }
 
 export function FileCardInfo({ filePreview, persistentId }: FileCardInfoProps) {
@@ -32,12 +21,15 @@ export function FileCardInfo({ filePreview, persistentId }: FileCardInfoProps) {
           {DateHelper.toDisplayFormat(filePreview.metadata.depositDate)} -{' '}
           <LinkToPage
             page={Route.DATASETS}
-            searchParams={getSearchParams(persistentId, filePreview.datasetPublishingStatus)}>
+            searchParams={FileCardHelper.getDatasetSearchParams(
+              persistentId,
+              filePreview.datasetPublishingStatus
+            )}>
             {filePreview.datasetName}
           </LinkToPage>
         </span>
         <span className={styles.info}>
-          <Stack gap={1} direction={'horizontal'}>
+          <Stack gap={1} direction="horizontal">
             {filePreview.metadata.type.toDisplayFormat()} - {filePreview.metadata.size.toString()}
             <FileTabularData tabularData={filePreview.metadata.tabularData} />
             <FileChecksum checksum={filePreview.metadata.checksum} />
--- src/sections/collection/datasets-list/file-card/FileCardThumbnail.tsx
+++ src/sections/collection/datasets-list/file-card/FileCardThumbnail.tsx
@@ -3,30 +3,23 @@ import { LinkToPage } from '../../../shared/link-to-page/LinkToPage'
 import { Route } from '../../../Route.enum'
 import { FileThumbnail } from '../../../dataset/dataset-files/files-table/file-info/file-info-cell/file-info-data/file-thumbnail/FileThumbnail'
 import { FilePreview } from '../../../../files/domain/models/FilePreview'
-import { DatasetPublishingStatus } from '../../../../dataset/domain/models/Dataset'
+import { FileCardHelper } from './FileCardHelper'
 
 interface FileCardThumbnailProps {
   persistentId: string
   filePreview: FilePreview
   thumbnail?: string
 }
-function getSearchParams(
-  persistentId: string,
-  publishingStatus: DatasetPublishingStatus
-): Record<string, string> {
-  const params: Record<string, string> = { persistentId: persistentId }
-  if (publishingStatus === DatasetPublishingStatus.DRAFT) {
-    // TODO: Replace with const after merge of #442
-    params.version = 'DRAFT'
-  }
-  return params
-}
+
 export function FileCardThumbnail({ persistentId, filePreview }: FileCardThumbnailProps) {
   return (
     <div className={styles.thumbnail}>
       <LinkToPage
         page={Route.FILES}
-        searchParams={getSearchParams(persistentId, filePreview.datasetPublishingStatus)}>
+        searchParams={FileCardHelper.getFileSearchParams(
+          filePreview.id,
+          filePreview.datasetPublishingStatus
+        )}>
         <FileThumbnail file={filePreview} />
       </LinkToPage>
     </div>
