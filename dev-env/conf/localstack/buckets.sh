1f29b7cb78951b03461a38910672b27d25d96d08
GPortas <hey@gportas.me>
2024-08-15 12:59:56 +0100

    Added: action npmrc setup and cleanup

--- dev-env/conf/localstack/buckets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Creating S3 bucket..."
-awslocal s3 mb s3://mybucket || { echo "Failed to create S3 bucket"; exit 1; }
-
-echo "Applying CORS configuration..."
-awslocal s3api put-bucket-cors --bucket mybucket --cors-configuration file:///etc/localstack/init/ready.d/cors.json || { echo "Failed to apply CORS configuration"; exit 1; }
-
-echo "CORS configuration applied successfully"
