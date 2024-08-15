650cbcf3b355341e7ba53b590106e8503633f40d
GPortas <hey@gportas.me>
2024-08-15 12:58:30 +0100

    Changed: use remote s3 aws storage instead of containerized

--- dev-env/run-env.sh
+++ dev-env/run-env.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-echo "INFO - Configuring etc/hosts to support direct upload through containers..."
-
-HOSTS_FILE="/etc/hosts"
-HOSTNAME="localstack"
-IP_ADDRESS="127.0.0.1"
-
-add_host_entry() {
-    if grep -q "$HOSTNAME" "$HOSTS_FILE"; then
-        echo "$HOSTNAME is already present in $HOSTS_FILE"
-    else
-        echo "Adding $HOSTNAME to $HOSTS_FILE"
-        echo "$IP_ADDRESS $HOSTNAME" | sudo tee -a "$HOSTS_FILE" > /dev/null
-        echo "$HOSTNAME added successfully"
-    fi
-}
-
-add_host_entry
-
 export DATAVERSE_IMAGE_TAG=$1
 
 # To avoid timeout issues on frontend container startup
