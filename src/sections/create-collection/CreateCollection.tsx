f820c6fff31a0960973839ab3045f9611590b9de
Germán Saracca <gersaracca@gmail.com>
2024-08-16 11:43:27 -0300

    feat: control if user can create collection or dataset inside create collection and create dataset page

--- src/sections/create-collection/CreateCollection.tsx
+++ src/sections/create-collection/CreateCollection.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Alert } from '@iqss/dataverse-design-system'
 import { useCollection } from '../collection/useCollection'
 import { CollectionRepository } from '../../collection/domain/repositories/CollectionRepository'
 import { useLoading } from '../loading/LoadingContext'
@@ -10,6 +11,7 @@ import { CollectionForm, CollectionFormData } from './collection-form/Collection
 import { SeparationLine } from '../shared/layout/SeparationLine/SeparationLine'
 import { PageNotFound } from '../page-not-found/PageNotFound'
 import { CreateCollectionSkeleton } from './CreateCollectionSkeleton'
+import { useGetCollectionUserPermissions } from '../../shared/hooks/useGetCollectionUserPermissions'
 
 interface CreateCollectionProps {
   ownerCollectionId: string
@@ -29,18 +31,36 @@ export function CreateCollection({
     ownerCollectionId
   )
 
+  const { collectionUserPermissions, isLoading: isLoadingCollectionUserPermissions } =
+    useGetCollectionUserPermissions({
+      collectionIdOrAlias: ownerCollectionId,
+      collectionRepository: collectionRepository
+    })
+
+  const canUserAddCollection = Boolean(collectionUserPermissions?.canAddCollection)
+
   useEffect(() => {
-    if (!isLoadingCollection) {
+    if (!isLoadingCollection && !isLoadingCollectionUserPermissions) {
       setIsLoading(false)
     }
-  }, [isLoading, isLoadingCollection, setIsLoading])
+  }, [isLoading, isLoadingCollection, isLoadingCollectionUserPermissions, setIsLoading])
 
   if (!isLoadingCollection && !collection) {
     return <PageNotFound />
   }
 
-  if (isLoadingCollection || !collection) {
+  if (isLoadingCollection || isLoadingCollectionUserPermissions || !collection) {
     return <CreateCollectionSkeleton />
+  }
+
+  if (collectionUserPermissions && !canUserAddCollection) {
+    return (
+      <div className="pt-4" data-testid="not-allowed-to-create-collection-alert">
+        <Alert variant="danger" dismissible={false}>
+          {t('notAllowedToCreateCollection')}
+        </Alert>
+      </div>
+    )
   }
 
   const formDefaultValues: CollectionFormData = {
