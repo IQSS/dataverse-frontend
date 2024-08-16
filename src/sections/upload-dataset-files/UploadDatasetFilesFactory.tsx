797f2c79a771a3e084e42e5fbacdf09bfc75a832
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-08-16 12:55:10 +0200

    dataset files upload factory fix

--- src/sections/upload-dataset-files/UploadDatasetFilesFactory.tsx
+++ src/sections/upload-dataset-files/UploadDatasetFilesFactory.tsx
@@ -4,6 +4,7 @@ import { DatasetJSDataverseRepository } from '../../dataset/infrastructure/repos
 import { FileJSDataverseRepository } from '../../files/infrastructure/FileJSDataverseRepository'
 import { DatasetProvider } from '../dataset/DatasetProvider'
 import { UploadDatasetFiles } from './UploadDatasetFiles'
+import { DatasetNonNumericVersion } from '../../dataset/domain/models/Dataset'
 
 const datasetRepository = new DatasetJSDataverseRepository()
 const fileRepository = new FileJSDataverseRepository()
@@ -17,9 +18,12 @@ export class UploadDatasetFilesFactory {
 function UploadDatasetFilesWithSearchParams() {
   const [searchParams] = useSearchParams()
   const persistentId = searchParams.get('persistentId') ?? undefined
+  const version = searchParams.get('version') ?? DatasetNonNumericVersion.DRAFT
 
   return (
-    <DatasetProvider repository={datasetRepository} searchParams={{ persistentId: persistentId }}>
+    <DatasetProvider
+      repository={datasetRepository}
+      searchParams={{ persistentId: persistentId, version: version }}>
       <UploadDatasetFiles fileRepository={fileRepository} />
     </DatasetProvider>
   )
