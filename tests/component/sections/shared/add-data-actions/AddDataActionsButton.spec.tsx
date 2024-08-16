815e70aed2e00b2f5ce6152e114c1dd14c6eeab1
Germán Saracca <gersaracca@gmail.com>
2024-08-16 12:40:34 -0300

    test: collection add data dropdown visibility

--- tests/component/sections/shared/add-data-actions/AddDataActionsButton.spec.tsx
+++ tests/component/sections/shared/add-data-actions/AddDataActionsButton.spec.tsx
@@ -2,7 +2,7 @@ import AddDataActionsButton from '../../../../../src/sections/shared/add-data-ac
 
 describe('AddDataActionsButton', () => {
   it('renders the button', () => {
-    cy.customMount(<AddDataActionsButton />)
+    cy.customMount(<AddDataActionsButton canAddCollection={true} canAddDataset={true} />)
 
     cy.findByRole('button', { name: /Add Data/i }).should('exist')
     cy.findByRole('button', { name: /Add Data/ }).click()
@@ -11,7 +11,7 @@ describe('AddDataActionsButton', () => {
   })
 
   it('renders the new dataset button with the correct generated link', () => {
-    cy.customMount(<AddDataActionsButton />)
+    cy.customMount(<AddDataActionsButton canAddCollection={true} canAddDataset={true} />)
 
     cy.findByRole('button', { name: /Add Data/i }).click()
     cy.findByText('New Dataset')
@@ -21,11 +21,61 @@ describe('AddDataActionsButton', () => {
 
   it('renders the new dataset button with the correct generated link', () => {
     const collectionId = 'some-collection-id'
-    cy.customMount(<AddDataActionsButton collectionId={collectionId} />)
+    cy.customMount(
+      <AddDataActionsButton
+        collectionId={collectionId}
+        canAddCollection={true}
+        canAddDataset={true}
+      />
+    )
 
     cy.findByRole('button', { name: /Add Data/i }).click()
     cy.findByText('New Dataset')
       .should('be.visible')
       .should('have.attr', 'href', `/datasets/create?collectionId=${collectionId}`)
+  })
+
+  it('shows New Collection button enabled if user has permissions to create collection', () => {
+    cy.customMount(<AddDataActionsButton canAddCollection={true} canAddDataset={true} />)
+
+    cy.findByRole('button', { name: /Add Data/i }).as('addDataBtn')
+    cy.get('@addDataBtn').should('exist')
+    cy.get('@addDataBtn').click()
+    cy.findByRole('link', { name: 'New Collection' })
+      .should('be.visible')
+      .should('not.have.attr', 'aria-disabled', 'false')
+  })
+
+  it('shows New Dataset button enabled if user has permissions to create dataset', () => {
+    cy.customMount(<AddDataActionsButton canAddCollection={true} canAddDataset={true} />)
+
+    cy.findByRole('button', { name: /Add Data/i }).as('addDataBtn')
+    cy.get('@addDataBtn').should('exist')
+    cy.get('@addDataBtn').click()
+    cy.findByRole('link', { name: 'New Dataset' })
+      .should('be.visible')
+      .should('not.have.attr', 'aria-disabled', 'true')
+  })
+
+  it('shows New Collection button disabled if user does not have permissions to create collection', () => {
+    cy.customMount(<AddDataActionsButton canAddCollection={false} canAddDataset={true} />)
+
+    cy.findByRole('button', { name: /Add Data/i }).as('addDataBtn')
+    cy.get('@addDataBtn').should('exist')
+    cy.get('@addDataBtn').click()
+    cy.findByRole('link', { name: 'New Collection' })
+      .should('be.visible')
+      .should('have.attr', 'aria-disabled', 'true')
+  })
+
+  it('shows New Dataset button disabled if user does not have permissions to create dataset', () => {
+    cy.customMount(<AddDataActionsButton canAddCollection={true} canAddDataset={false} />)
+
+    cy.findByRole('button', { name: /Add Data/i }).as('addDataBtn')
+    cy.get('@addDataBtn').should('exist')
+    cy.get('@addDataBtn').click()
+    cy.findByRole('link', { name: 'New Dataset' })
+      .should('be.visible')
+      .should('have.attr', 'aria-disabled', 'true')
   })
 })
