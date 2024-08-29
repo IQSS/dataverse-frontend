6a5cbcc83c19ae0ef036f43600e018ef40fe7f1c
Germán Saracca <gersaracca@gmail.com>
2024-08-29 14:05:25 -0300

    feat: search input styles done

--- src/sections/homepage/Homepage.tsx
+++ src/sections/homepage/Homepage.tsx
@@ -1,6 +1,8 @@
 import { SearchInput } from './search-input/SearchInput'
 import styles from './Homepage.module.scss'
 
+// TODO:ME - New footer only for the homepage, replicate harvard installation? under discussion
+
 export const Homepage = () => {
   return (
     <section className={styles['section-wrapper']}>
--- src/sections/homepage/search-input/SearchInput.tsx
+++ src/sections/homepage/search-input/SearchInput.tsx
@@ -1,13 +1,29 @@
+import { useRef, useState } from 'react'
 import { Form } from '@iqss/dataverse-design-system'
-import styles from './SearchInput.module.scss'
+import { CloseButton } from 'react-bootstrap'
 import { Search as SearchIcon } from 'react-bootstrap-icons'
+import styles from './SearchInput.module.scss'
 
 export const SearchInput = () => {
+  const inputSearchRef = useRef<HTMLInputElement>(null)
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('Search:', searchValue)
+  }
+
+  const handleClearSearch = () => {
+    setSearchValue('')
+    inputSearchRef.current?.focus()
+  }
+
   return (
-    <div className={styles['search-input-wrapper']}>
-      <div className={styles['search-icon-wrapper']}>
-        <SearchIcon size={22} />
-      </div>
+    <Form onSubmit={handleSubmit} className={styles['search-input-wrapper']}>
       <Form.Group.Input
         type="text"
         role="search"
@@ -15,7 +31,21 @@ export const SearchInput = () => {
         autoFocus
         autoComplete="off"
         className={styles['text-input']}
+        value={searchValue}
+        onChange={handleSearchChange}
+        ref={inputSearchRef}
       />
-    </div>
+      {searchValue && (
+        <CloseButton
+          aria-label="Clear search"
+          className={styles['clear-btn']}
+          onClick={handleClearSearch}
+        />
+      )}
+
+      <button type="submit" aria-label="Submit Search" className={styles['search-btn']}>
+        <SearchIcon size={22} />
+      </button>
+    </Form>
   )
 }
