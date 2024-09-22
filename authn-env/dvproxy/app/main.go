d929e0c0fda8d3d9a8177ca42b938127ab7e15e9
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-09-22 20:17:15 +0200

    docker compose with keycloack, oauth2-proxy and dvproxy

---
+++ authn-env/dvproxy/app/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/libis/rdm-dataverse-go-api/api"
+)
+
+const timeout = 5 * time.Minute
+
+var dataverseServer = os.Getenv("DATAVERSE_SERVER")
+var apiKey = os.Getenv("API_KEY")
+var unblockKey = os.Getenv("UNBLOCK_KEY")
+var userHeader = os.Getenv("USER_HEADER")
+
+func main() {
+	srvMux := http.NewServeMux()
+	srvMux.Handle("/", http.HandlerFunc(handle))
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	srv := &http.Server{
+		Addr:              ":8080",
+		ReadTimeout:       timeout,
+		WriteTimeout:      timeout,
+		IdleTimeout:       timeout,
+		ReadHeaderTimeout: timeout,
+		TLSConfig:         tlsConfig,
+		Handler:           http.TimeoutHandler(srvMux, timeout, fmt.Sprintf("processing the request took longer than %v: cancelled", timeout)),
+	}
+	srv.ListenAndServe()
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	client := api.NewClient(dataverseServer)
+	fmt.Println(r.Header)
+	client.User = r.Header.Get(userHeader)
+	client.AdminApiKey = apiKey
+	client.UnblockKey = unblockKey
+	req := client.NewRequest(r.URL.Path, r.Method, r.Body, r.Header)
+	res, err := api.DoStream(r.Context(), req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer res.Close()
+	io.Copy(w, res)
+}
---
+++ authn-env/dvproxy/go.mod
@@ -0,0 +1,5 @@
+module dvproxy
+
+go 1.22
+
+require github.com/libis/rdm-dataverse-go-api v1.0.7
