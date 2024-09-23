8fa72e5ec224130685b1e696a679c10973b0cb46
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-09-23 14:36:14 +0200

    login link

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = (import.meta.env.VITE_DATAVERSE_BACKEND_URL as string) ?? ''
+export const AUTHN_URL = (import.meta.env.VITE_DATAVERSE_AUTHN_URL as string) ?? BASE_URL
--- src/sections/layout/header/Header.tsx
+++ src/sections/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { Navbar } from '@iqss/dataverse-design-system'
 import { Route } from '../../Route.enum'
 import { useSession } from '../../session/SessionContext'
-import { BASE_URL } from '../../../config'
+import { AUTHN_URL, BASE_URL } from '../../../config'
 import { LoggedInHeaderActions } from './LoggedInHeaderActions'
 import { CollectionJSDataverseRepository } from '../../../collection/infrastructure/repositories/CollectionJSDataverseRepository'
 import styles from './Header.module.scss'
@@ -26,7 +26,7 @@ export function Header() {
         <LoggedInHeaderActions user={user} collectionRepository={collectionRepository} />
       ) : (
         <>
-          <Navbar.Link href={`${BASE_URL}${Route.LOG_IN}`}>{t('logIn')}</Navbar.Link>
+          <Navbar.Link href={`${AUTHN_URL}${Route.LOG_IN}`}>{t('logIn')}</Navbar.Link>
           <Navbar.Link href={`${BASE_URL}${Route.SIGN_UP}`}>{t('signUp')}</Navbar.Link>
         </>
       )}
