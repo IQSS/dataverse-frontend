746a19f6a99e14c16b0a5e8e1c64e8c15e35375a
Germán Saracca <gersaracca@gmail.com>
2024-09-24 09:29:48 -0300

    chore: add src folder alias to improve imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import istanbul from 'vite-plugin-istanbul'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,8 @@ export default defineConfig({
   ],
   preview: {
     port: 5173
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   }
 })
