bee9a0d8c56691b963d11a5ac0c1d7679e721cd4
Germán Saracca <gersaracca@gmail.com>
2024-09-27 16:34:35 -0300

    feat: app loader full screen for initial suspense

--- src/sections/shared/layout/app-loader/AppLoader.tsx
+++ src/sections/shared/layout/app-loader/AppLoader.tsx
@@ -1,9 +1,17 @@
+import cn from 'classnames'
 import { Spinner } from '@iqss/dataverse-design-system'
 import styles from './AppLoader.module.scss'
 
-export const AppLoader = () => {
+interface AppLoaderProps {
+  fullViewport?: boolean
+}
+
+export const AppLoader = ({ fullViewport = false }: AppLoaderProps) => {
   return (
-    <section className={styles['app-loader']}>
+    <section
+      className={cn(styles['app-loader'], {
+        [styles['full-viewport']]: fullViewport
+      })}>
       <Spinner />
     </section>
   )
