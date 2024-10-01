8ad3f1a492a52a0ae964380b2b21ca1d84dca3df
Germán Saracca <gersaracca@gmail.com>
2024-10-01 08:45:48 -0300

    added TODOs for using time tag for displaying dates

--- src/sections/dataset/dataset-files/files-table/file-info/file-info-cell/file-info-data/FileDate.tsx
+++ src/sections/dataset/dataset-files/files-table/file-info/file-info-cell/file-info-data/FileDate.tsx
@@ -2,6 +2,8 @@ import { FileDate as FileDateModel } from '../../../../../../../files/domain/mod
 import { useTranslation } from 'react-i18next'
 import { DateHelper } from '../../../../../../../shared/helpers/DateHelper'
 
+// TODO: use time tag with dateTime attr https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+
 export function FileDate({ date }: { date: FileDateModel }) {
   const { t } = useTranslation('files')
   return (
