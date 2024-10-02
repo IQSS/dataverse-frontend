ce6bbfcba96079facdf4859301ef1b841c5021c0
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-10-02 10:24:34 +0200

    trying different config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = (import.meta.env.VITE_DATAVERSE_BACKEND_URL as string) ?? ''
+export const BASE_URL = `${window.location.protocol}//${window.location.hostname}`
 export const AUTHN_URL = (import.meta.env.VITE_DATAVERSE_AUTHN_URL as string) ?? BASE_URL
