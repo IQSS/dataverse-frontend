336b11f3725ce53488f79caa6ed8e8a38ca9be5e
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-10-02 21:23:52 +0200

    finally working config

--- dev-env/add-keycloak-in-hosts.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo '127.0.1.1       keycloak2.mydomain.com' >> /etc/hosts
--- dev-env/add-oidc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST -H 'Content-type: application/json' --upload-file oidc.json http://localhost:8080/api/admin/authenticationProviders
