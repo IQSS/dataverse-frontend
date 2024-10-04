d11f55e64fe0ac6b2d10125058fdaf0c339d13b6
Germán Saracca <gersaracca@gmail.com>
2024-10-04 17:14:09 -0300

    feat: login, logout and user name in header with OIDC AuthContext functions and data

--- src/sections/layout/header/Header.tsx
+++ src/sections/layout/header/Header.tsx
@@ -1,9 +1,10 @@
+import { useContext } from 'react'
+import { AuthContext } from 'react-oauth2-code-pkce'
 import dataverse_logo from '../../../assets/dataverse_brand_icon.svg'
 import { useTranslation } from 'react-i18next'
-import { Navbar } from '@iqss/dataverse-design-system'
+import { Button, Navbar } from '@iqss/dataverse-design-system'
 import { Route } from '../../Route.enum'
-import { useSession } from '../../session/SessionContext'
-import { BASE_URL } from '../../../config'
+// import { BASE_URL } from '../../../config'
 import { LoggedInHeaderActions } from './LoggedInHeaderActions'
 import { CollectionJSDataverseRepository } from '../../../collection/infrastructure/repositories/CollectionJSDataverseRepository'
 import styles from './Header.module.scss'
@@ -12,7 +13,13 @@ const collectionRepository = new CollectionJSDataverseRepository()
 
 export function Header() {
   const { t } = useTranslation('header')
-  const { user } = useSession()
+
+  // tokenData is originally typed as Record<string, any> but we know it has a name property (this will need a double check in future iterations)
+  const { token, tokenData, logIn: oidcLogin } = useContext(AuthContext)
+
+  const handleOidcLogIn = () => {
+    oidcLogin()
+  }
 
   return (
     <Navbar
@@ -22,12 +29,18 @@ export function Header() {
         logoImgSrc: dataverse_logo
       }}
       className={styles.navbar}>
-      {user ? (
-        <LoggedInHeaderActions user={user} collectionRepository={collectionRepository} />
+      {token && tokenData ? (
+        <LoggedInHeaderActions
+          userName={tokenData?.name as string}
+          collectionRepository={collectionRepository}
+        />
       ) : (
         <>
-          <Navbar.Link href={`${BASE_URL}${Route.LOG_IN}`}>{t('logIn')}</Navbar.Link>
-          <Navbar.Link href={`${BASE_URL}${Route.SIGN_UP}`}>{t('signUp')}</Navbar.Link>
+          <Button onClick={handleOidcLogIn} variant="link" className={styles['login-btn']}>
+            OIDC {t('logIn')}
+          </Button>
+          {/* <Navbar.Link href={`${BASE_URL}${Route.LOG_IN}`}>{t('logIn')}</Navbar.Link> */}
+          {/* <Navbar.Link href={`${BASE_URL}${Route.SIGN_UP}`}>{t('signUp')}</Navbar.Link> */}
         </>
       )}
     </Navbar>
--- src/sections/layout/header/LoggedInHeaderActions.tsx
+++ src/sections/layout/header/LoggedInHeaderActions.tsx
@@ -1,10 +1,11 @@
+import { useContext } from 'react'
+import { AuthContext } from 'react-oauth2-code-pkce'
 import { useTranslation } from 'react-i18next'
 import { Link, useNavigate } from 'react-router-dom'
 import { Navbar } from '@iqss/dataverse-design-system'
 import { useGetCollectionUserPermissions } from '../../../shared/hooks/useGetCollectionUserPermissions'
 import { useSession } from '../../session/SessionContext'
 import { RouteWithParams, Route } from '../../Route.enum'
-import { User } from '../../../users/domain/models/User'
 import { CollectionRepository } from '../../../collection/domain/repositories/CollectionRepository'
 import { ROOT_COLLECTION_ALIAS } from '../../../collection/domain/models/Collection'
 import { AccountHelper } from '../../account/AccountHelper'
@@ -12,24 +13,31 @@ import { AccountHelper } from '../../account/AccountHelper'
 const currentPage = 0
 
 interface LoggedInHeaderActionsProps {
-  user: User
+  userName: string
   collectionRepository: CollectionRepository
 }
 
 export const LoggedInHeaderActions = ({
-  user,
+  userName,
   collectionRepository
 }: LoggedInHeaderActionsProps) => {
   const { t } = useTranslation('header')
   const { logout } = useSession()
   const navigate = useNavigate()
 
+  const { logOut: oidcLogout } = useContext(AuthContext)
+
   const { collectionUserPermissions } = useGetCollectionUserPermissions({
     collectionIdOrAlias: ROOT_COLLECTION_ALIAS,
     collectionRepository: collectionRepository
   })
 
-  const onLogoutClick = () => {
+  // Just keeping logOut in a handler function because we might want to add more logic here (e.g: logOut can receive up to 3 parameters)
+  const handleOidcLogout = () => {
+    oidcLogout()
+  }
+
+  const _handleSessionLogout = () => {
     void logout().then(() => {
       navigate(currentPage)
     })
@@ -54,15 +62,18 @@ export const LoggedInHeaderActions = ({
           {t('navigation.newDataset')}
         </Navbar.Dropdown.Item>
       </Navbar.Dropdown>
-      <Navbar.Dropdown title={user.displayName} id="dropdown-user">
+      <Navbar.Dropdown title={userName} id="dropdown-user">
         <Navbar.Dropdown.Item
           as={Link}
           to={`${Route.ACCOUNT}?${AccountHelper.ACCOUNT_PANEL_TAB_QUERY_KEY}=${AccountHelper.ACCOUNT_PANEL_TABS_KEYS.apiToken}`}>
           {t('navigation.apiToken')}
         </Navbar.Dropdown.Item>
-        <Navbar.Dropdown.Item href="#" onClick={onLogoutClick}>
-          {t('logOut')}
+        <Navbar.Dropdown.Item href="#" onClick={handleOidcLogout}>
+          OIDC {t('logOut')}
         </Navbar.Dropdown.Item>
+        {/* <Navbar.Dropdown.Item href="#" onClick={handleSessionLogout}>
+          {t('logOut')}
+        </Navbar.Dropdown.Item> */}
       </Navbar.Dropdown>
     </>
   )
