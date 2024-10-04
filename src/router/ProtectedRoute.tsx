1edfce948bdb8b08aecffbc53c5bd83ecf82286b
Germán Saracca <gersaracca@gmail.com>
2024-10-04 16:47:35 -0300

    feat: modify protected route base on AuthContext properties

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,23 +1,18 @@
+import { useContext } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Route } from '../sections/Route.enum'
-import { useSession } from '../sections/session/SessionContext'
+import { AuthContext } from 'react-oauth2-code-pkce'
 import { AppLoader } from '../sections/shared/layout/app-loader/AppLoader'
-import { BASE_URL } from '../config'
 
 export const ProtectedRoute = () => {
-  const { user, isLoadingUser } = useSession()
+  const { token, loginInProgress, logIn } = useContext(AuthContext)
 
-  if (isLoadingUser) {
+  if (loginInProgress) {
     return <AppLoader />
   }
 
-  if (!user) {
-    window.location.href = `${BASE_URL}${Route.LOG_IN}`
-    return null
+  if (!token) {
+    logIn()
   }
-
-  // When we have the login page inside the SPA, we can use the following code:
-  // return !user ? <Navigate to="/login" replace /> : <Outlet />
 
   return <Outlet />
 }
