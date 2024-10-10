b6587f23f709f2b8b86f908b0b6486af1158d11f
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2024-10-09 23:29:22 -0400

    test:  add more unit tests

--- tests/component/sections/dataset/dataset-publish/PublishDatasetModal.spec.tsx
+++ tests/component/sections/dataset/dataset-publish/PublishDatasetModal.spec.tsx
@@ -58,6 +58,33 @@ describe('PublishDatasetModal', () => {
     // Check if the error message is displayed
     cy.contains(errorMessage).should('exist')
   })
+  it('displays an error message when publishCollection fails', () => {
+    const handleClose = cy.stub()
+    const repository = {} as DatasetRepository // Mock the repository as needed
+    const collectionRepository = {} as CollectionRepository
+    collectionRepository.publish = cy
+      .stub()
+      .as('collectionRepositoryPublish')
+      .rejects(new Error('collection error'))
+    const parentCollection = UpwardHierarchyNodeMother.createCollection({ isReleased: false })
+    cy.mountAuthenticated(
+      <PublishDatasetModal
+        show={true}
+        repository={repository}
+        collectionRepository={collectionRepository}
+        parentCollection={parentCollection}
+        persistentId="testPersistentId"
+        releasedVersionExists={false}
+        handleClose={handleClose}
+      />
+    )
+
+    // Trigger the Publish action
+    cy.findByText('Continue').click()
+
+    // Check if the error message is displayed
+    cy.contains('collection error').should('exist')
+  })
   it('renders the PublishDatasetModal for previously released dataset and triggers submitPublish on button click', () => {
     const handleClose = cy.stub()
     const repository = {} as DatasetRepository // Mock the repository as needed
