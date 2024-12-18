363a83153eaea0b1f8dc0d26c271347343fe3ba6
Germán Saracca <gersaracca@gmail.com>
2024-12-18 14:40:44 -0300

    feat: add story

---
+++ src/stories/collection-featured-items/CollectionFeaturedItems.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { CollectionFeaturedItems } from '@/sections/collection-featured-items/CollectionFeaturedItems'
+import { WithI18next } from '../WithI18next'
+import { WithLayout } from '../WithLayout'
+import { WithLoggedInUser } from '../WithLoggedInUser'
+import { CollectionMockRepository } from '../collection/CollectionMockRepository'
+import { CollectionLoadingMockRepository } from '../collection/CollectionLoadingMockRepository'
+
+const meta: Meta<typeof CollectionFeaturedItems> = {
+  title: 'Pages/Collection Featured Items',
+  component: CollectionFeaturedItems,
+  decorators: [WithI18next, WithLayout, WithLoggedInUser],
+  parameters: {
+    // Sets the delay for all stories.
+    chromatic: { delay: 15000, pauseAnimationAtEnd: true }
+  }
+}
+export default meta
+type Story = StoryObj<typeof CollectionFeaturedItems>
+
+export const Default: Story = {
+  render: () => (
+    <CollectionFeaturedItems
+      collectionRepository={new CollectionMockRepository()}
+      collectionIdFromParams="root"
+    />
+  )
+}
+export const Loading: Story = {
+  render: () => (
+    <CollectionFeaturedItems
+      collectionRepository={new CollectionLoadingMockRepository()}
+      collectionIdFromParams="root"
+    />
+  )
+}
