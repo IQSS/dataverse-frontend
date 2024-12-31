636970bd02bbd4b810c71bf7a935050abbe90bb7
Germán Saracca <gersaracca@gmail.com>
2024-12-31 10:41:50 -0300

    chore: remove cypress-real-events package

--- tests/support/component.ts
+++ tests/support/component.ts
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '@cypress/code-coverage/support'
-import 'cypress-real-events'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 // Alternatively you can use CommonJS syntax:
