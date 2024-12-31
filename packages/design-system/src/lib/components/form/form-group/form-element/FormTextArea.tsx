b11b21d25f46f619d71ca3a2225c8d5762c3c70f
Germán Saracca <gersaracca@gmail.com>
2024-12-31 11:57:51 -0300

    feat(ds): revert remove row prop

--- packages/design-system/src/lib/components/form/form-group/form-element/FormTextArea.tsx
+++ packages/design-system/src/lib/components/form/form-group/form-element/FormTextArea.tsx
@@ -2,24 +2,22 @@ import { Form as FormBS } from 'react-bootstrap'
 import * as React from 'react'
 
 export type FormInputElement = HTMLInputElement | HTMLTextAreaElement
-export interface FormTextAreaProps extends React.HTMLAttributes<FormInputElement> {
+export interface FormTextAreaProps extends Omit<React.HTMLAttributes<FormInputElement>, 'rows'> {
   name?: string
   disabled?: boolean
   isValid?: boolean
   isInvalid?: boolean
   value?: string
   autoFocus?: boolean
-  rows?: number
 }
 
 export const FormTextArea = React.forwardRef(function FormTextArea(
-  { name, disabled, isValid, isInvalid, value, autoFocus, rows = 5, ...props }: FormTextAreaProps,
+  { name, disabled, isValid, isInvalid, value, autoFocus, ...props }: FormTextAreaProps,
   ref
 ) {
   return (
     <FormBS.Control
       as="textarea"
-      rows={rows}
       disabled={disabled}
       name={name}
       isValid={isValid}
