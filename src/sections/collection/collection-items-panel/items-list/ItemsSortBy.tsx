35745afac081afab757478835cacadedbf27b710
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-01-06 13:23:34 -0500

    fix: use hasSearchValue instead of currentSearchText; move helper functions below component function

--- src/sections/collection/collection-items-panel/items-list/ItemsSortBy.tsx
+++ src/sections/collection/collection-items-panel/items-list/ItemsSortBy.tsx
@@ -17,21 +17,75 @@ export enum SortOption {
 interface ItemsSortByProps {
   currentSortType?: SortType
   currentSortOrder?: OrderType
-  currentSearchText?: string
+  hasSearchValue?: boolean
   onSortChange: (newSortType?: SortType, newOrderType?: OrderType) => void
   isLoadingCollectionItems: boolean
+}
+
+export function ItemsSortBy({
+  currentSortType,
+  currentSortOrder,
+  onSortChange,
+  hasSearchValue,
+  isLoadingCollectionItems
+}: ItemsSortByProps) {
+  const { t } = useTranslation('collection')
+  const [selectedOption, setSelectedOption] = useState<SortOption>(
+    convertToSortOption(currentSortType, currentSortOrder, hasSearchValue)
+  )
+  useEffect(() => {
+    const newSortOption = convertToSortOption(currentSortType, currentSortOrder, hasSearchValue)
+    if (newSortOption !== selectedOption) {
+      setSelectedOption(newSortOption)
+    }
+  }, [currentSortType, currentSortOrder, hasSearchValue, selectedOption])
+
+  const handleSortChange = (eventKey: string | null) => {
+    const newSortOption = eventKey as SortOption
+    if (selectedOption !== newSortOption) {
+      setSelectedOption(newSortOption)
+      onSortChange(
+        convertFromSortOption(newSortOption).sortType,
+        convertFromSortOption(newSortOption).orderType
+      )
+    }
+  }
+
+  const sortOptions = Object.values(SortOption).filter(
+    (sortByOption) => sortByOption !== SortOption.RELEVANCE || hasSearchValue
+  )
+
+  return (
+    <DropdownButton
+      icon={<ArrowDownUp className={styles.icon} role="img" aria-label={t('sort.title')} />}
+      title={t('sort.title')}
+      id="collection-items-sort"
+      variant="secondary"
+      disabled={isLoadingCollectionItems}
+      onSelect={handleSortChange}>
+      {Object.values(sortOptions).map((sortByOption) => (
+        <DropdownButtonItem
+          key={sortByOption}
+          eventKey={sortByOption}
+          aria-selected={selectedOption === sortByOption}
+          className={selectedOption === sortByOption ? styles['selected-sort-option'] : ''}>
+          {t(`sort.options.${sortByOption}`)}
+        </DropdownButtonItem>
+      ))}
+    </DropdownButton>
+  )
 }
 function convertToSortOption(
   sortType?: SortType,
   orderType?: OrderType,
-  searchText?: string
+  hasSearchValue?: boolean
 ): SortOption {
   let sortOption: SortOption
   if (sortType === SortType.NAME) {
     sortOption = orderType === OrderType.ASC ? SortOption.NAME_ASC : SortOption.NAME_DESC
   } else if (sortType === SortType.DATE) {
     sortOption = orderType === OrderType.ASC ? SortOption.DATE_ASC : SortOption.DATE_DESC
-  } else if (searchText) {
+  } else if (hasSearchValue) {
     sortOption = SortOption.RELEVANCE
   } else {
     sortOption = SortOption.DATE_DESC
@@ -54,58 +108,4 @@ function convertFromSortOption(sortOption: SortOption): {
     case SortOption.RELEVANCE:
       return { sortType: undefined, orderType: undefined }
   }
-}
-
-export function ItemsSortBy({
-  currentSortType,
-  currentSortOrder,
-  onSortChange,
-  currentSearchText,
-  isLoadingCollectionItems
-}: ItemsSortByProps) {
-  const { t } = useTranslation('collection')
-  const [selectedOption, setSelectedOption] = useState<SortOption>(
-    convertToSortOption(currentSortType, currentSortOrder, currentSearchText)
-  )
-  useEffect(() => {
-    const newSortOption = convertToSortOption(currentSortType, currentSortOrder, currentSearchText)
-    if (newSortOption !== selectedOption) {
-      setSelectedOption(newSortOption)
-    }
-  }, [currentSortType, currentSortOrder, currentSearchText])
-
-  const handleSortChange = (eventKey: string | null) => {
-    const newSortOption = eventKey as SortOption
-    if (selectedOption !== newSortOption) {
-      setSelectedOption(newSortOption)
-      onSortChange(
-        convertFromSortOption(newSortOption).sortType,
-        convertFromSortOption(newSortOption).orderType
-      )
-    }
-  }
-
-  const sortOptions = Object.values(SortOption).filter(
-    (sortByOption) => sortByOption !== SortOption.RELEVANCE || currentSearchText !== undefined
-  )
-
-  return (
-    <DropdownButton
-      icon={<ArrowDownUp className={styles.icon} role="img" aria-label={t('sort.title')} />}
-      title={t('sort.title')}
-      id="collection-items-sort"
-      variant="secondary"
-      disabled={isLoadingCollectionItems}
-      onSelect={handleSortChange}>
-      {Object.values(sortOptions).map((sortByOption) => (
-        <DropdownButtonItem
-          key={sortByOption}
-          eventKey={sortByOption}
-          aria-selected={selectedOption === sortByOption}
-          className={selectedOption === sortByOption ? styles['selected-sort-option'] : ''}>
-          {t(`sort.options.${sortByOption}`)}
-        </DropdownButtonItem>
-      ))}
-    </DropdownButton>
-  )
 }
