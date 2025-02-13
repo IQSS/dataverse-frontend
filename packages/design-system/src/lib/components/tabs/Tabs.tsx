3f84eab571aa3fed8f525854320bd04290a1a1b0
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-02-12 20:17:33 -0500

    fix: added activeKey prop to Tabs

--- packages/design-system/src/lib/components/tabs/Tabs.tsx
+++ packages/design-system/src/lib/components/tabs/Tabs.tsx
@@ -3,13 +3,20 @@ import { Tab } from './Tab'
 import { Tabs as TabsBS } from 'react-bootstrap'
 
 interface TabsProps {
-  defaultActiveKey: string
+  defaultActiveKey?: string
+  activeKey?: string
   onSelect?: (key: string | null) => void
 }
 
-function Tabs({ defaultActiveKey, onSelect, children }: PropsWithChildren<TabsProps>) {
+function Tabs({ defaultActiveKey, activeKey, onSelect, children }: PropsWithChildren<TabsProps>) {
+  if (activeKey && !onSelect) {
+    throw new Error('Tabs component requires onSelect function when activeKey is provided')
+  }
+  if (!activeKey && !defaultActiveKey) {
+    throw new Error('Tabs component requires either activeKey or defaultActiveKey')
+  }
   return (
-    <TabsBS onSelect={onSelect} activeKey={defaultActiveKey}>
+    <TabsBS onSelect={onSelect} activeKey={activeKey} defaultActiveKey={defaultActiveKey}>
       {children}
     </TabsBS>
   )
