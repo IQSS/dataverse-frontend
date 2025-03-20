5821b261c3b9b1806b6307222caa644894926d99
Germán Saracca <gersaracca@gmail.com>
2025-03-20 11:28:38 -0300

    fix: invalidate cache for img

--- src/sections/featured-item/featured-item-view/FeaturedItemView.tsx
+++ src/sections/featured-item/featured-item-view/FeaturedItemView.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import DOMPurify from 'dompurify'
 import { CustomFeaturedItem } from '@/collection/domain/models/CollectionFeaturedItem'
 import { BreadcrumbsGenerator } from '@/sections/shared/hierarchy/BreadcrumbsGenerator'
@@ -22,7 +23,16 @@ export const FeaturedItemView = ({
   collectionHierarchy
 }: FeaturedItemViewProps) => {
   const showTopBreadcrumb = showBreadcrumbs && !featuredItem.imageFileUrl
-  const showBanner = featuredItem.imageFileUrl
+
+  const imageUrl = useMemo(() => {
+    if (!featuredItem.imageFileUrl) return null
+
+    return featuredItem.imageFileUrl.startsWith('blob:')
+      ? featuredItem.imageFileUrl
+      : appendTimestampToImageUrl(featuredItem.imageFileUrl)
+  }, [featuredItem.imageFileUrl])
+
+  const showBanner = imageUrl
 
   const sanitizedContent = DOMPurify.sanitize(featuredItem.content, {
     USE_PROFILES: { html: true }
@@ -40,7 +50,7 @@ export const FeaturedItemView = ({
 
       {showBanner && (
         <div className={styles.img_banner}>
-          <img src={featuredItem.imageFileUrl} alt="featured item image" />
+          <img src={imageUrl} alt="featured item image" />
 
           {showBreadcrumbs && (
             <div className={styles.breadcrumbs_in_banner_wrapper}>
@@ -58,3 +68,13 @@ export const FeaturedItemView = ({
     </section>
   )
 }
+
+/*
+  NOTE:
+  To avoid image caching issues, for example after just changing the image of an existing featured item (image url will keep the same) we add a
+  timestamp as query param to the image URL so we force the browser to fetch the image again.
+  When this component is used in the edit collection featured items form previewer, the imageFileUrl is a local generated blob URL, in that case
+  we don't need to add the timestamp.
+*/
+export const appendTimestampToImageUrl = (imageUrl: string) =>
+  `${imageUrl}?timestamp-to-invalidate-cache=${Date.now()}`
