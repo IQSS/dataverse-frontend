a62b8c0dd0864a03c0951323db91fd52405b4257
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-03-24 19:39:10 -0400

    fix: EditDatasetMenu.stories.tsx

--- src/stories/dataset/dataset-action-buttons/edit-dataset-menu/EditDatasetMenu.stories.tsx
+++ src/stories/dataset/dataset-action-buttons/edit-dataset-menu/EditDatasetMenu.stories.tsx
@@ -3,15 +3,18 @@ import { WithI18next } from '../../../WithI18next'
 import { WithSettings } from '../../../WithSettings'
 import {
   DatasetMother,
-  DatasetPermissionsMother
+  DatasetPermissionsMother,
+  DatasetVersionMother
 } from '../../../../../tests/component/dataset/domain/models/DatasetMother'
 import { DatasetMockRepository } from '../../../dataset/DatasetMockRepository'
 import { EditDatasetMenu } from '../../../../sections/dataset/dataset-action-buttons/edit-dataset-menu/EditDatasetMenu'
+import { WithLoggedInUser } from '@/stories/WithLoggedInUser'
+import { DatasetPublishingStatus } from '@/dataset/domain/models/Dataset'
 
 const meta: Meta<typeof EditDatasetMenu> = {
   title: 'Sections/Dataset Page/DatasetActionButtons/EditDatasetMenu',
   component: EditDatasetMenu,
-  decorators: [WithI18next, WithSettings],
+  decorators: [WithI18next, WithSettings, WithLoggedInUser],
   parameters: {
     // Sets the delay for all stories.
     chromatic: { delay: 15000, pauseAnimationAtEnd: true }
@@ -27,6 +30,22 @@ export const WithAllPermissions: Story = {
       datasetRepository={new DatasetMockRepository()}
       dataset={DatasetMother.create({
         permissions: DatasetPermissionsMother.createWithAllAllowed(),
+        hasValidTermsOfAccess: true
+      })}
+    />
+  )
+}
+
+export const WithAllPermissionsDraftVersionOnly: Story = {
+  render: () => (
+    <EditDatasetMenu
+      datasetRepository={new DatasetMockRepository()}
+      dataset={DatasetMother.create({
+        version: DatasetVersionMother.createDraft({
+          latestVersionPublishingStatus: DatasetPublishingStatus.DRAFT
+        }),
+        permissions: DatasetPermissionsMother.createWithAllAllowed(),
+        versionsSummaries: [],
         hasValidTermsOfAccess: true
       })}
     />
