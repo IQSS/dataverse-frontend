0a426309dada88f0e2fc6f16e9d8320396db5d74
Germán Saracca <gersaracca@gmail.com>
2025-04-10 13:12:26 -0300

    fix?

--- src/stories/sign-up/SignUp.stories.tsx
+++ src/stories/sign-up/SignUp.stories.tsx
@@ -2,13 +2,13 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { WithLayout } from '../WithLayout'
 import { WithI18next } from '../WithI18next'
 import { SignUp } from '@/sections/sign-up/SignUp'
-import { WithOIDCAuthContext } from '../WithOIDCAuthContext'
-import { UserMockRepository } from '../shared-mock-repositories/user/UserMockRepository'
+// import { WithOIDCAuthContext } from '../WithOIDCAuthContext'
+// import { UserMockRepository } from '../shared-mock-repositories/user/UserMockRepository'
 
 const meta: Meta<typeof SignUp> = {
   title: 'Pages/Sign Up',
   component: SignUp,
-  decorators: [WithI18next, WithLayout, WithOIDCAuthContext],
+  decorators: [WithI18next, WithLayout],
   parameters: {
     // Sets the delay for all stories.
     chromatic: { delay: 15000, pauseAnimationAtEnd: true }
@@ -19,6 +19,7 @@ type Story = StoryObj<typeof SignUp>
 
 export const ValidTokenWithNotLinkedAccount: Story = {
   render: () => (
-    <SignUp userRepository={new UserMockRepository()} hasValidTokenButNotLinkedAccount />
+    <div>holo</div>
+    // <SignUp userRepository={new UserMockRepository()} hasValidTokenButNotLinkedAccount />
   )
 }
