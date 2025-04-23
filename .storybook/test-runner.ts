6fa4e09f624c96ba2dd0927f45d28e5db77e0bae
Germán Saracca <gersaracca@gmail.com>
2025-04-23 17:38:25 -0300

    fix: move the wait for to the postVisit

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -5,26 +5,19 @@ import { getStoryContext } from '@storybook/test-runner'
 import type { TestRunnerConfig } from '@storybook/test-runner'
 
 const a11yConfig: TestRunnerConfig = {
-  async preVisit(page, context) {
-    const { parameters } = await getStoryContext(page, context)
-
-    // Wait for the Story
-    if (parameters.waitForSelector) {
-      console.dir({ waitForSelector: parameters.waitForSelector })
-      await page.waitForSelector(parameters.waitForSelector, {
-        timeout: 15_000
-      })
-    }
-    // await page.locator(parameters.waitForSelector).waitFor({ state: 'attached', timeout: 15_000 })
-
-    // if (tags.includes('wait-attached')) {
-    //   await page.locator('#kc-login-template').waitFor({ state: 'attached', timeout: 15_000 })
-    // }
-
+  async preVisit(page) {
     await injectAxe(page)
   },
   async postVisit(page, context) {
     const storyContext = await getStoryContext(page, context)
+
+    // Wait for the Story
+    if (storyContext.parameters?.waitForSelector) {
+      console.dir({ waitForSelector: storyContext.parameters.waitForSelector })
+      await page.waitForSelector(storyContext.parameters.waitForSelector, {
+        timeout: 15_000
+      })
+    }
 
     await configureAxe(page, {
       rules: storyContext.parameters?.a11y?.config?.rules
