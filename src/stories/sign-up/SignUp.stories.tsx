977a863a147179a124a9b3786ec9caf241def874
Germán Saracca <gersaracca@gmail.com>
2025-05-06 09:59:02 -0300

    feat: add stories of loading and error

--- src/stories/sign-up/SignUp.stories.tsx
+++ src/stories/sign-up/SignUp.stories.tsx
@@ -5,6 +5,8 @@ import { SignUp } from '@/sections/sign-up/SignUp'
 import { WithOIDCAuthContext } from '../WithOIDCAuthContext'
 import { UserMockRepository } from '../shared-mock-repositories/user/UserMockRepository'
 import { DataverseInfoMockRepository } from '../shared-mock-repositories/info/DataverseInfoMockRepository'
+import { DataverseInfoMockLoadingRepository } from '../shared-mock-repositories/info/DataverseInfoMockLoadingkRepository'
+import { DataverseInfoMockErrorRepository } from '../shared-mock-repositories/info/DataverseInfoMockErrorRepository'
 
 const meta: Meta<typeof SignUp> = {
   title: 'Pages/Sign Up',
@@ -23,6 +25,26 @@ export const ValidTokenWithNotLinkedAccount: Story = {
     <SignUp
       userRepository={new UserMockRepository()}
       dataverseInfoRepository={new DataverseInfoMockRepository()}
+      hasValidTokenButNotLinkedAccount
+    />
+  )
+}
+
+export const LoadingTermsOfUse: Story = {
+  render: () => (
+    <SignUp
+      userRepository={new UserMockRepository()}
+      dataverseInfoRepository={new DataverseInfoMockLoadingRepository()}
+      hasValidTokenButNotLinkedAccount
+    />
+  )
+}
+
+export const FailedToFetchTermsOfUse: Story = {
+  render: () => (
+    <SignUp
+      userRepository={new UserMockRepository()}
+      dataverseInfoRepository={new DataverseInfoMockErrorRepository()}
       hasValidTokenButNotLinkedAccount
     />
   )
