989cadfca9fd9653cd27b2974136578acfba010e
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2025-05-13 16:53:43 -0400

    remove role filter

--- src/sections/collection/collection-items-panel/filter-panel/FilterPanel.tsx
+++ src/sections/collection/collection-items-panel/filter-panel/FilterPanel.tsx
@@ -7,24 +7,15 @@ import { ItemTypeChange, TypeFilters } from './type-filters/TypeFilters'
 import { CollectionItemsFacet } from '@/collection/domain/models/CollectionItemSubset'
 import { FilterQuery } from '@/collection/domain/models/CollectionSearchCriteria'
 import { FacetsFilters } from './facets-filters/FacetsFilters'
-import { RoleFilters } from '@/sections/account/my-data-section/my-data-filter-panel/role-filters/RoleFilters'
 import { RemoveAddFacetFilter } from './facets-filters/FacetFilterGroup'
-import { RoleChange } from '@/sections/account/my-data-section/my-data-filter-panel/role-filters/RoleFilters'
 import styles from './FilterPanel.module.scss'
-import { Role } from '@/users/domain/models/Role'
 
-interface RoleFilterProps {
-  userRoles: Role[]
-  currentUserRoleIds: number[]
-  onRolesChange: (roleChange: RoleChange) => void
-}
 interface FilterPanelProps {
   currentItemTypes?: CollectionItemType[]
   onItemTypesChange: (itemTypeChange: ItemTypeChange) => void
   facets: CollectionItemsFacet[]
   currentFilterQueries?: FilterQuery[]
   onFacetChange: (filterQuery: FilterQuery, removeOrAdd: RemoveAddFacetFilter) => void
-  roleFilterProps?: RoleFilterProps
   isLoadingCollectionItems: boolean
 }
 
@@ -34,7 +25,6 @@ export const FilterPanel = ({
   facets,
   currentFilterQueries,
   onFacetChange,
-  roleFilterProps,
   isLoadingCollectionItems
 }: FilterPanelProps) => {
   const { t } = useTranslation('collection')
@@ -65,21 +55,12 @@ export const FilterPanel = ({
               currentItemTypes={currentItemTypes}
               isLoadingCollectionItems={isLoadingCollectionItems}
             />
-
             <FacetsFilters
               facets={facets}
               currentFilterQueries={currentFilterQueries}
               onFacetChange={onFacetChange}
               isLoadingCollectionItems={isLoadingCollectionItems}
             />
-            {roleFilterProps && (
-              <RoleFilters
-                currentRoleIds={roleFilterProps.currentUserRoleIds}
-                userRoles={roleFilterProps.userRoles}
-                onRolesChange={roleFilterProps.onRolesChange}
-                isLoadingCollectionItems={isLoadingCollectionItems}
-              />
-            )}
           </div>
         </Offcanvas.Body>
       </Offcanvas>
