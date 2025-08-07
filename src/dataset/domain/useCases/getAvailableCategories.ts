18435b1fd1f252736d14d41ac18966f28f19be14
Cheng Shi <cks5520psu@gmail.com>
2025-08-07 09:33:18 -0400

    fix: prevent creating new error

--- src/dataset/domain/useCases/getAvailableCategories.ts
+++ src/dataset/domain/useCases/getAvailableCategories.ts
@@ -1,11 +1,8 @@
-import { ReadError } from '@iqss/dataverse-client-javascript'
 import { DatasetRepository } from '../repositories/DatasetRepository'
 
 export async function getAvailableCategories(
   datasetRepository: DatasetRepository,
   datasetId: string | number
 ): Promise<string[]> {
-  return datasetRepository.getAvailableCategories(datasetId).catch((error: ReadError) => {
-    throw new Error(error.message)
-  })
+  return datasetRepository.getAvailableCategories(datasetId)
 }
