ccbe452359aa87775461ebb3c82a7e748232fc33
Germán Saracca <gersaracca@gmail.com>
2025-08-13 09:35:16 -0300

    add spa banner

--- src/sections/layout/Layout.tsx
+++ src/sections/layout/Layout.tsx
@@ -11,6 +11,13 @@ export function Layout() {
     <HistoryTrackerProvider>
       <TopBarProgressIndicator />
       {HeaderFactory.create()}
+      <div className="alert alert-warning rounded-0" role="alert">
+        <div className="container">
+          You are using the new Dataverse <strong>SPA version</strong>. This is an early release and
+          some features from the original site are not yet available.
+        </div>
+      </div>
+
       <main>
         <Container className={styles['body-container']}>
           <Outlet />
