900f7fe51282920a512787f8bcdac8707e255300
Germán Saracca <gersaracca@gmail.com>
2025-09-02 15:25:27 -0300

    feat: initial setup

---
+++ src/dataset/domain/models/DatasetTemplate.ts
@@ -0,0 +1,29 @@
+import { DatasetLicense, DatasetMetadataFieldValue, DatasetTermsOfUse } from './Dataset'
+
+export interface DatasetTemplate {
+  id: number
+  name: string
+  alias: string
+  isDefault: boolean
+  usageCount: number
+  createTime: string
+  createDate: string
+  // 👇 From Edit Template Metadata
+  datasetFields: DatasetFields
+  instructions: DatasetTemplateInstruction[]
+  // 👇 From Edit Template Terms
+  termsOfUse: DatasetTermsOfUse
+  license?: DatasetLicense // This license property is going to be present if not custom terms are added in the UI
+}
+
+type DatasetFields = Record<string, DatasetFieldInfo>
+
+interface DatasetFieldInfo {
+  displayName: string
+  name: string
+  fields: DatasetMetadataFieldValue[]
+}
+export interface DatasetTemplateInstruction {
+  instructionField: string
+  instructionText: string
+}
