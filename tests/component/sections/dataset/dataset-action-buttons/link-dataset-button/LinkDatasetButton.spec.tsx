c197b79c7bd9719e3f37c6f1ad0fb8aed0d74908
Germán Saracca <gersaracca@gmail.com>
2025-09-18 17:47:18 -0300

    test: add unit cases for linking dataset

--- tests/component/sections/dataset/dataset-action-buttons/link-dataset-button/LinkDatasetButton.spec.tsx
+++ tests/component/sections/dataset/dataset-action-buttons/link-dataset-button/LinkDatasetButton.spec.tsx
@@ -3,14 +3,42 @@ import {
   DatasetVersionMother
 } from '../../../../dataset/domain/models/DatasetMother'
 import { LinkDatasetButton } from '../../../../../../src/sections/dataset/dataset-action-buttons/link-dataset-button/LinkDatasetButton'
+import { CollectionRepository } from '@/collection/domain/repositories/CollectionRepository'
+import { DatasetRepository } from '@/dataset/domain/repositories/DatasetRepository'
+import { ReadError, WriteError } from '@iqss/dataverse-client-javascript'
+import { CollectionSummaryMother } from '@tests/component/collection/domain/models/CollectionSummaryMother'
+import { CollectionMockRepository } from '@/stories/collection/CollectionMockRepository'
+
+const collectionRepository: CollectionRepository = {} as CollectionRepository
+const datasetRepository: DatasetRepository = {} as DatasetRepository
 
 describe('LinkDatasetButton', () => {
+  beforeEach(() => {
+    cy.viewport('macbook-15')
+    // Default stubs for common happy-path
+    datasetRepository.link = cy.stub().as('linkDataset').resolves()
+    datasetRepository.getDatasetLinkedCollections = cy
+      .stub()
+      .as('getDatasetLinkedCollections')
+      .resolves([])
+    collectionRepository.getForLinking = cy
+      .stub()
+      .as('getCollectionsForLinking')
+      .resolves(CollectionSummaryMother.createManyRealistic(5))
+  })
+
   it('renders the LinkDatasetButton if the user is authenticated and the dataset version is not deaccessioned and the dataset is released', () => {
     const dataset = DatasetMother.create({
       version: DatasetVersionMother.createReleased()
     })
 
-    cy.mountAuthenticated(<LinkDatasetButton dataset={dataset} />)
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
 
     cy.findByRole('button', { name: 'Link Dataset' }).should('exist')
   })
@@ -20,7 +48,13 @@ describe('LinkDatasetButton', () => {
       version: DatasetVersionMother.createReleased()
     })
 
-    cy.customMount(<LinkDatasetButton dataset={dataset} />)
+    cy.customMount(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
 
     cy.findByRole('button', { name: 'Link Dataset' }).should('not.exist')
   })
@@ -30,8 +64,377 @@ describe('LinkDatasetButton', () => {
       version: DatasetVersionMother.createDeaccessioned()
     })
 
-    cy.mountAuthenticated(<LinkDatasetButton dataset={dataset} />)
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
 
     cy.findByRole('button', { name: 'Link Dataset' }).should('not.exist')
+  })
+
+  it('selects a collection and links the dataset successfully', () => {
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        // Save disabled without selection
+        cy.findByRole('button', { name: /Save Linked Dataset/i }).should('be.disabled')
+
+        cy.findByLabelText(/Toggle options menu/)
+          .should('exist')
+          .click()
+        cy.findByText('Collection 3').should('exist').click()
+
+        cy.findByRole('button', { name: /Save Linked Dataset/i })
+          .should('not.be.disabled')
+          .click()
+
+        cy.get('@linkDataset').should((spy) => {
+          const linkSpy = spy as unknown as Cypress.Agent<sinon.SinonSpy>
+          const datasetIdArg = linkSpy.getCall(0).args[0] as string
+          const linkingCollectionIdArg = linkSpy.getCall(0).args[1] as number
+
+          expect(datasetIdArg).to.equal(dataset.persistentId)
+          expect(linkingCollectionIdArg).to.equal(3)
+        })
+      })
+
+    // Success toast should appear
+    cy.findByText(/Dataset from .* has been successfully linked to/i)
+      .should('exist')
+      .should('be.visible')
+  })
+
+  it('searches for a collection to link', () => {
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={new CollectionMockRepository()}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByRole('button', { name: /Save Linked Dataset/i }).should('be.disabled')
+
+        cy.findByLabelText(/Toggle options menu/)
+          .should('exist')
+          .click()
+
+        // No results
+        cy.findByPlaceholderText('Enter Collection Name').type('XYZ')
+        cy.wait(500)
+        cy.findByText('No collections found').should('exist')
+
+        // Matching result
+        cy.findByPlaceholderText('Enter Collection Name').clear().type('Collection 3')
+        cy.wait(500)
+        cy.findByText('Collection 3').should('exist')
+      })
+  })
+
+  it('shows no collections to link message', () => {
+    collectionRepository.getForLinking = cy.stub().resolves([])
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByText(/No collections are showing up to link./)
+          .should('exist')
+          .should('be.visible')
+      })
+
+    cy.findByRole('button', { name: /Save Linked Dataset/i }).should('be.disabled')
+  })
+
+  it('shows only one collection message and auto-selects it', () => {
+    collectionRepository.getForLinking = cy.stub().resolves([
+      CollectionSummaryMother.create({
+        id: 555,
+        displayName: 'Bar Collection',
+        alias: 'bar-collection'
+      })
+    ])
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByText(/You have one collection you can add linked collection and datasets in./)
+          .should('exist')
+          .should('be.visible')
+
+        cy.findByLabelText('Your Collection')
+          .should('have.value', 'Bar Collection')
+          .and('have.attr', 'readonly')
+      })
+
+    cy.findByRole('button', { name: /Save Linked Dataset/i })
+      .should('not.be.disabled')
+      .click()
+
+    cy.get('@linkDataset').should((spy) => {
+      const linkSpy = spy as unknown as Cypress.Agent<sinon.SinonSpy>
+      const datasetIdArg = linkSpy.getCall(0).args[0] as string
+      const linkingCollectionIdArg = linkSpy.getCall(0).args[1] as number
+
+      expect(datasetIdArg).to.equal(dataset.persistentId)
+      expect(linkingCollectionIdArg).to.equal(555)
+    })
+  })
+
+  it('handles error when linking the dataset fails', () => {
+    datasetRepository.link = cy.stub().as('linkDataset').rejects(new Error('Linking failed'))
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByLabelText(/Toggle options menu/)
+          .should('exist')
+          .click()
+        cy.findByText('Collection 2').should('exist').click()
+
+        cy.findByRole('button', { name: /Save Linked Dataset/i })
+          .should('not.be.disabled')
+          .click()
+
+        cy.get('@linkDataset').should((spy) => {
+          const linkSpy = spy as unknown as Cypress.Agent<sinon.SinonSpy>
+          const datasetIdArg = linkSpy.getCall(0).args[0] as string
+          const linkingCollectionIdArg = linkSpy.getCall(0).args[1] as number
+          expect(datasetIdArg).to.equal(dataset.persistentId)
+          expect(linkingCollectionIdArg).to.equal(2)
+        })
+
+        cy.findByText(/An unexpected error occurred while linking the dataset./)
+          .should('exist')
+          .should('be.visible')
+      })
+
+    cy.findByText(/Dataset from .* has been successfully linked to/i).should('not.exist')
+  })
+
+  it('handles error when linking fails with WriteError', () => {
+    datasetRepository.link = cy
+      .stub()
+      .as('linkDataset')
+      .rejects(new WriteError('A WriteError received'))
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByLabelText(/Toggle options menu/)
+          .should('exist')
+          .click()
+        cy.findByText('Collection 2').should('exist').click()
+
+        cy.findByRole('button', { name: /Save Linked Dataset/i })
+          .should('not.be.disabled')
+          .click()
+
+        cy.get('@linkDataset').should((spy) => {
+          const linkSpy = spy as unknown as Cypress.Agent<sinon.SinonSpy>
+          const datasetIdArg = linkSpy.getCall(0).args[0] as string
+          const linkingCollectionIdArg = linkSpy.getCall(0).args[1] as number
+          expect(datasetIdArg).to.equal(dataset.persistentId)
+          expect(linkingCollectionIdArg).to.equal(2)
+        })
+
+        cy.findByText(/A WriteError received/i)
+          .should('exist')
+          .should('be.visible')
+      })
+
+    cy.findByText(/Dataset from .* has been successfully linked to/i).should('not.exist')
+  })
+
+  it('handles error when fetching collections for linking fails', () => {
+    collectionRepository.getForLinking = cy.stub().rejects(new Error('Fetching collections failed'))
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByText(/An unexpected error occurred while fetching collections for linking./)
+          .should('exist')
+          .should('be.visible')
+      })
+  })
+
+  it('handles error when fetching collections fails with ReadError', () => {
+    collectionRepository.getForLinking = cy.stub().rejects(new ReadError('A ReadError received'))
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByText(/A ReadError received/i)
+          .should('exist')
+          .should('be.visible')
+      })
+  })
+
+  it('shows already linked collections for the dataset when opening the modal', () => {
+    const linked = [
+      CollectionSummaryMother.create({ id: 7, displayName: 'Alpha', alias: 'alpha' }),
+      CollectionSummaryMother.create({ id: 8, displayName: 'Beta', alias: 'beta' })
+    ]
+    datasetRepository.getDatasetLinkedCollections = cy
+      .stub()
+      .as('getDatasetLinkedCollections')
+      .resolves(linked)
+
+    const dataset = DatasetMother.create({
+      version: DatasetVersionMother.createReleased()
+    })
+
+    cy.mountAuthenticated(
+      <LinkDatasetButton
+        dataset={dataset}
+        datasetRepository={datasetRepository}
+        collectionRepository={collectionRepository}
+      />
+    )
+
+    cy.findByRole('button', { name: /Link Dataset/i })
+      .should('exist')
+      .click()
+
+    cy.get('@getDatasetLinkedCollections').should((spy) => {
+      const getSpy = spy as unknown as Cypress.Agent<sinon.SinonSpy>
+      const datasetIdArg = getSpy.getCall(0).args[0] as number | string
+      expect(datasetIdArg).to.equal(dataset.id)
+    })
+
+    cy.findByRole('dialog')
+      .should('be.visible')
+      .within(() => {
+        cy.findByText(/Note: This dataset is already linked to the following collections:/).should(
+          'exist'
+        )
+        cy.findByText('Alpha,').should('exist')
+        cy.findByText('Beta.').should('exist')
+      })
   })
 })
