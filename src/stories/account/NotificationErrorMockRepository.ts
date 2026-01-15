70d75e64e08e755d614d72c7535251af60886ef0
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2026-01-15 14:36:51 -0500

    fix lint warnings

--- src/stories/account/NotificationErrorMockRepository.ts
+++ src/stories/account/NotificationErrorMockRepository.ts
@@ -1,12 +1,8 @@
 import { NotificationRepository } from '@/notifications/domain/repositories/NotificationRepository'
-import { Notification } from '@/notifications/domain/models/Notification'
-import { NotificationMother } from '@tests/component/notifications/domain/models/NotificationMother'
 import { FakerHelper } from '@tests/component/shared/FakerHelper'
 import { NotificationSubset } from '@/notifications/domain/models/NotificationSubset'
 
 export class NotificationErrorMockRepository implements NotificationRepository {
-  private notifications: Notification[] = NotificationMother.createManyRealistic()
-
   async getAllNotificationsByUser(): Promise<NotificationSubset> {
     return new Promise((_resolve, reject) => {
       setTimeout(() => {
@@ -23,7 +19,7 @@ export class NotificationErrorMockRepository implements NotificationRepository {
     })
   }
 
-  async markNotificationAsRead(notificationId: number): Promise<void> {
+  async markNotificationAsRead(_notificationId: number): Promise<void> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         reject('Something went wrong getting notifications. Try again later.')
@@ -31,7 +27,7 @@ export class NotificationErrorMockRepository implements NotificationRepository {
     })
   }
 
-  deleteNotification(notificationId: number): Promise<void> {
+  deleteNotification(_notificationId: number): Promise<void> {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         reject('Something went wrong getting notifications. Try again later.')
