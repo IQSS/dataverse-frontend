c67095cc023ad46b0588da8ba76463879a35073f
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2026-02-18 10:12:11 -0500

    remove unused trimmedLength

--- src/info/infrastructure/repositories/DataverseInfoJSDataverseRepository.ts
+++ src/info/infrastructure/repositories/DataverseInfoJSDataverseRepository.ts
@@ -109,8 +109,6 @@ export class DataverseInfoJSDataverseRepository implements DataverseInfoReposito
     return getPublishDatasetDisclaimerText
       .execute()
       .then((text) => {
-        const trimmedLength = text?.trim().length ?? 0
-
         return {
           name: SettingName.PUBLISH_DATASET_DISCLAIMER_TEXT,
           value: text
